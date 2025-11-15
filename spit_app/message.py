9a428532d0b9b030a3dd05c73627d4148e4a1e46
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-11-15 16:41:03 +0000

    Replace app with self

--- spit_app/message.py
+++ spit_app/message.py
@@ -3,65 +3,65 @@ from textual.widgets import Markdown, Static
 from textual.containers import VerticalScroll
 import spit_app.latex_math as lm
 
-def is_y_max(app) -> None:
-    app.chat_view_y_max = False
-    if app.chat_view.scroll_y + 1 >= app.chat_view.max_scroll_y:
-        app.chat_view_y_max = True
+def is_y_max(self) -> None:
+    self.chat_view_y_max = False
+    if self.chat_view.scroll_y + 1 >= self.chat_view.max_scroll_y:
+        self.chat_view_y_max = True
 
-def if_y_max_scroll_end(app) -> None:
-    if app.chat_view_y_max:
-        app.chat_view.scroll_end(animate=False)
+def if_y_max_scroll_end(self) -> None:
+    if self.chat_view_y_max:
+        self.chat_view.scroll_end(animate=False)
 
-async def mount(app, mtype: str, content: str = "") -> None:
-    is_y_max(app)
-    if app.edit:
-        app.message_container = app.edit_container
+async def mount(self, mtype: str, content: str = "") -> None:
+    is_y_max(self)
+    if self.edit:
+        self.message_container = self.edit_container
     else:
-        id=len(app.chat_view.children)
-        if len(app.state) > 0 and app.state[0]["role"] == "system":
+        id=len(self.chat_view.children)
+        if len(self.state) > 0 and self.state[0]["role"] == "system":
             id+=1
-        app.message_container = VerticalScroll(classes="message-container-"+mtype, id="id_"+str(id))
-    app.mwidget = Markdown(classes=mtype)
-    app.mtype = mtype
-    if not app.edit:
-        await app.chat_view.mount(app.message_container)
-    await app.message_container.mount(app.mwidget)
-    app.message_container.focus()
-    app.focused_message = app.message_container
+        self.message_container = VerticalScroll(classes="message-container-"+mtype, id="id_"+str(id))
+    self.mwidget = Markdown(classes=mtype)
+    self.mtype = mtype
+    if not self.edit:
+        await self.chat_view.mount(self.message_container)
+    await self.message_container.mount(self.mwidget)
+    self.message_container.focus()
+    self.focused_message = self.message_container
     if content:
-        await app.mwidget.update(content)
-    if_y_max_scroll_end(app)
+        await self.mwidget.update(content)
+    if_y_max_scroll_end(self)
 
-async def mount_next(app) -> None:
-    is_y_max(app)
-    app.mwidget = Markdown(classes=app.mtype)
-    await app.message_container.mount(app.mwidget)
-    if_y_max_scroll_end(app)
+async def mount_next(self) -> None:
+    is_y_max(self)
+    self.mwidget = Markdown(classes=self.mtype)
+    await self.message_container.mount(self.mwidget)
+    if_y_max_scroll_end(self)
 
-async def update(app, content: str) -> None:
-    is_y_max(app)
-    slen=len(app.mwidget.source)
-    if app.mwidget.source == content[:slen]:
-        await app.mwidget.append(content[slen:])
+async def update(self, content: str) -> None:
+    is_y_max(self)
+    slen=len(self.mwidget.source)
+    if self.mwidget.source == content[:slen]:
+        await self.mwidget.append(content[slen:])
     else:
-        await app.mwidget.update(content)
-    if_y_max_scroll_end(app)
+        await self.mwidget.update(content)
+    if_y_max_scroll_end(self)
 
-async def remove(app) -> None:
-    is_y_max(app)
-    await app.mwidget.remove()
-    if_y_max_scroll_end(app)
+async def remove(self) -> None:
+    is_y_max(self)
+    await self.mwidget.remove()
+    if_y_max_scroll_end(self)
 
-async def mount_latex(app, latex_image: Image) -> None:
-    is_y_max(app)
-    await app.message_container.mount(latex_image)
-    if_y_max_scroll_end(app)
+async def mount_latex(self, latex_image: Image) -> None:
+    is_y_max(self)
+    await self.message_container.mount(latex_image)
+    if_y_max_scroll_end(self)
 
-async def remove_last_turn(app) -> None:
-    if app.chat_view.children:
-        is_y_max(app)
-        if app.chat_view.children[-1] == app.focused_message:
-            if len(app.chat_view.children) > 1:
-                app.focused_message = app.chat_view.children[-2]
-        await app.chat_view.children[-1].remove()
-        if_y_max_scroll_end(app)
+async def remove_last_turn(self) -> None:
+    if self.chat_view.children:
+        is_y_max(self)
+        if self.chat_view.children[-1] == self.focused_message:
+            if len(self.chat_view.children) > 1:
+                self.focused_message = self.chat_view.children[-2]
+        await self.chat_view.children[-1].remove()
+        if_y_max_scroll_end(self)
