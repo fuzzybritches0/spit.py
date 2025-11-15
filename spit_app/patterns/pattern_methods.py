f69a442c71e5726553d224e1fac28eb87b62c9d9
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-11-15 09:50:54 +0000

    patterns: ignore <br>

--- spit_app/patterns/pattern_methods.py
+++ spit_app/patterns/pattern_methods.py
@@ -105,6 +105,10 @@ def escape(self, buffer: str, pattern: str) -> None:
     self.escapeS = not self.escapeS
 
 def escape_ltgt(self, buffer: str, pattern: str) -> None:
+    if buffer.startswith("<br>"):
+        return None
+    if self.paragraph.endswith("<br"):
+        return None
     if pattern == "<" and self.pp_next.isalnum():
         self.paragraph+="\\"
     elif pattern == ">" and self.pp_last.isalnum():
