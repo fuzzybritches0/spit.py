3797c34e525e5d47a3760bd0ae9db32bd5984583
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-11-23 19:44:35 +0000

    helpers: dot2obj
    Add a helper method that creates the necessary object structure to store
    values in any arbitrary depth. Examples:
    - chat_template_kwargs.reasoning_effort
    - chat_template_kwargs.model_identity

---
+++ spit_app/helpers/dot2obj.py
@@ -0,0 +1,9 @@
+def dot2obj(data: dict, dotpath: str, value: str|int|float) -> None:
+    path = dotpath.split(".")
+    cur = data
+    for key in path[:-1]:
+        if key not in cur or not isinstance(cur[key], dict):
+            cur[key] = {}
+        cur = cur[key]
+    cur[path[-1]] = value
+    return data
