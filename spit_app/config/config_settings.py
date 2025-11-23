6022953ee0d2832211858dcad283e176e37ec361
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-11-23 20:06:22 +0000

    config: settings: add edit settings functionality
    We adapt to the new config structure and simplify the store method. We
    now only accept an empty value as the default value - which here means,
    the setting of the value will not be included in the JSON payload, when
    empty. The structure of the settings is now stored in
    self.configs[count]["custom"]. This has implications we need to
    circumvent should the user decide to manually modify the settings file.
    Add methods to remove and add custom settings.

--- spit_app/config/config_settings.py
+++ spit_app/config/config_settings.py
@@ -22,42 +22,77 @@ class ConfigSettings:
             raise e
 
     def init(self) -> None:
-        self.config = { "active_config": 0, "configs": [] }
-        self.config["configs"].append({"name": "default config 0", "endpoint_url": None})
+        self.active_config = 0
+        self.configs = []
+        self.new()
 
     def save(self) -> None:
-        self.config_file.write_text(json.dumps(self.config))
+        config = {}
+        config["active_config"] = self.active_config
+        config["configs"] = self.configs
+        self.config_file.write_text(json.dumps(config))
 
-    def store(self, cconfig: int, setting: str, stype: int | float | str, value: str) -> None:
-        if (value.lower() == "none" or value.lower() == "default" or value == ""):
-            value = None
-        elif stype == float:
+    def store(self, cconfig: int, setting: str, stype: str, value: str | bool) -> None:
+        if stype == "Float" and value:
             value = float(value)
-        elif stype == int:
+        elif stype == "Integer" and value:
             value = int(value)
-        self.config["configs"][cconfig][setting] = value
+        self.configs[cconfig]["values"][setting] = value
         
     def load(self) -> None:
         if self.config_file.exists():
-            self.config = json.loads(self.config_file.read_text())
+            config = json.loads(self.config_file.read_text())
+            self.active_config = config["active_config"]
+            self.configs = config["configs"]
         else:
             self.init()
-        self.config["tools"] = self.read_tool_desc()
+        self.tools = self.read_tool_desc()
 
     def new(self) -> int:
-        ccount = len(self.config["configs"])
-        self.config["configs"].append({"name": f"default config {ccount}", "endpoint_url": None})
+        ccount = len(self.configs)
+        self.configs.append(
+            {
+                "values": {
+                    "name": f"default config {ccount}",
+                    "endpoint_url": "http://127.0.0.1:8080",
+                    "key": None
+                },
+                "custom": [
+                    ("name", "String", "Name", []),
+                    ("endpoint_url", "String", "Endpoint URL", []),
+                    ("key", "String", "API Access Key", []),
+                    ("temperature", "Float", "Temperature", []),
+                    ("top_p", "Float", "TOP-P", []),
+                    ("min_p", "Float", "MIN-P", []),
+                    ("top_k", "Float", "TOP-K", [])
+                ]
+            }
+        )
         return ccount
 
     def set_active(self, conf: int) -> None:
-        self.config["active_config"] = conf
+        self.active_config = conf
         self.save()
 
     def delete_config(self, conf: int) -> None:
-        active = self.config["active_config"]
-        del self.config["configs"][conf]
+        active = self.active_config
+        del self.configs[conf]
         if active == conf:
-            self.config["active_config"] = 0
-        if len(self.config["configs"]) < 1:
+            self.active_config = 0
+        if len(self.configs) < 1:
             self.init()
         self.save()
+
+    def remove_custom_setting(self, conf: int, rsetting: str) -> None:
+        custom = []
+        values = {}
+        for setting, stype, desc, sarray in self.configs[conf]["custom"]:
+            if not rsetting == setting:
+                custom.append((setting, stype, desc, sarray))
+                if setting in self.configs[conf]["values"]:
+                    values[setting] = self.configs[conf]["values"][setting]
+        self.configs[conf]["custom"] = custom
+        self.configs[conf]["values"] = values
+
+    def add_custom_setting(self, conf: int, setting: str, stype: str, desc: str, sarray: list) -> None:
+        self.configs[conf]["custom"].append((setting, stype, desc, sarray))
