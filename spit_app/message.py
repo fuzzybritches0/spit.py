825ef3bacc12b4b0d9aea7cc0e4e9d3ec2704b41
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-11-29 17:39:39 +0000

    message: try widget.anchor() method again
    The workaround we used around app.chat_view.anchor() seems to yield the
    same result as app.chat_view.anchor(). Neither is working perfectly.
    Let's drop the workaround and investigate further later on.

--- spit_app/message.py
+++ spit_app/message.py
@@ -4,17 +4,7 @@ from textual.widgets import Markdown, Static
 from textual.containers import VerticalScroll
 import spit_app.latex_math as lm
 
-def is_y_max(self) -> None:
-    self.chat_view_y_max = False
-    if self.chat_view.scroll_y + 1 >= self.chat_view.max_scroll_y:
-        self.chat_view_y_max = True
-
-def if_y_max_scroll_end(self) -> None:
-    if self.chat_view_y_max:
-        self.chat_view.scroll_end(animate=False)
-
 async def mount(self, mtype: str, content: str = "") -> None:
-    is_y_max(self)
     if self.edit:
         self.message_container = self.edit_container
         id = int(self.message_container.id[3:])
@@ -36,30 +26,22 @@ async def mount(self, mtype: str, content: str = "") -> None:
     self.focused_message = self.message_container
     if content:
         await self.mwidget.update(content)
-    if_y_max_scroll_end(self)
 
 async def mount_next(self) -> None:
-    is_y_max(self)
     self.mwidget = Markdown(classes=self.mtype)
     await self.message_container.mount(self.mwidget)
-    if_y_max_scroll_end(self)
 
 async def update(self, content: str) -> None:
-    is_y_max(self)
     slen=len(self.mwidget.source)
     if self.mwidget.source == content[:slen]:
         await self.mwidget.append(content[slen:])
     else:
         await self.mwidget.update(content)
-    if_y_max_scroll_end(self)
 
 async def remove(self) -> None:
-    is_y_max(self)
     await self.mwidget.remove()
-    if_y_max_scroll_end(self)
 
 async def mount_code(self) -> None:
-    is_y_max(self)
     turn_id=len(self.code_listings)-1
     code_id=len(self.code_listings[turn_id])
     id="code-listing-"+str(turn_id)+"-"+str(code_id)
@@ -67,25 +49,20 @@ async def mount_code(self) -> None:
     self.mwidget = Markdown(classes=self.mtype)
     await self.message_container.mount(self.code_listing_container)
     await self.code_listing_container.mount(self.mwidget)
-    if_y_max_scroll_end(self)
 
 async def mount_latex(self, latex_image: Image) -> None:
-    is_y_max(self)
     turn_id=len(self.latex_listings)-1
     latex_id=len(self.latex_listings[turn_id])
     id="latex-listing-"+str(turn_id)+"-"+str(latex_id)
     self.latex_listing_container = VerticalScroll(classes="code-listing-"+self.mtype, id=id)
     await self.message_container.mount(self.latex_listing_container)
     await self.latex_listing_container.mount(latex_image)
-    if_y_max_scroll_end(self)
 
 async def remove_last_turn(self) -> None:
     if self.chat_view.children:
-        is_y_max(self)
         if self.chat_view.children[-1] == self.focused_message:
             if len(self.chat_view.children) > 1:
                 self.focused_message = self.chat_view.children[-2]
         await self.chat_view.children[-1].remove()
         del self.code_listings[-1]
         del self.latex_listings[-1]
-        if_y_max_scroll_end(self)
