866e98497f892bbc02219d5448a282bef169ef7f
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 20:28:47 +0000

    chat: render: move methods into new file
    Move all methods staring with render into new file render and rename
    them without the leading render.

---
+++ spit_app/chat/render.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: GPL-2.0
+import json
+import spit_app.chat.message as mmessage
+
+async def messages(self) -> None:
+    for umessage in self.messages:
+        await message(self, umessage)
+
+async def message(self, umessage, scroll_visible: bool = False) -> None:
+    if umessage["role"] == "user" and umessage["content"]:
+        await _message(self, "request", umessage["content"], scroll_visible)
+    elif umessage["role"] == "assistant" and "content" in umessage and umessage["content"]:
+        await _message(self, "response", umessage["content"], scroll_visible)
+    elif umessage["role"] == "assistant" and "tool_calls" in umessage and umessage["tool_calls"]:
+        await _tool_calls(self, umessage["tool_calls"], scroll_visible)
+    elif umessage["role"] == "tool" and "content" in umessage and umessage["content"]:
+        await _tool_response(self, umessage["content"], scroll_visible)
+
+async def _tool_calls(self, tool_calls, scroll_visible: bool) -> None:
+    for tool_call in tool_calls:
+        await _message(self, "response",
+                              "- TOOL CALL: `" + json.dumps(tool_call) + "`",
+                              scroll_visible)
+
+async def _tool_response(self, tool_response, scroll_visible: bool) -> None:
+    await _message(self, "request",
+                          "- RESULT: `" + json.dumps(tool_response) + "`",
+                          scroll_visible)
+
+async def _message(self, mtype: str, messagec: str, scroll_visible: bool) -> None:
+    buffer = ""
+    pp = self.pattern_processing(self)
+    await mmessage.mount(self, mtype, scroll_visible)
+    for char in messagec:
+        buffer += char
+        if len(buffer) < 8:
+            continue
+        await pp.process_patterns(False, buffer)
+        if pp.skip_buff_p > 0:
+            pp.skip_buff_p -= 1
+        else:
+            pp.paragraph += buffer[:1]
+        buffer = buffer[1:]
+
+    for char in buffer:
+        await pp.process_patterns(False, buffer)
+        if pp.skip_buff_p > 0:
+            pp.skip_buff_p -= 1
+        else:
+            pp.paragraph += buffer[:1]
+        buffer = buffer[1:]
+    if not pp.paragraph:
+        await mmessage.remove(self)
+    else:
+        await mmessage.update(self, pp.paragraph)
