d1d99d78a77c4347fe6227ea0c44277847562b41
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 20:30:43 +0000

    chat: undo: move methods into new file
    Move all undo/redo methods into a separate file.

---
+++ spit_app/chat/undo.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: GPL-2.0
+import spit_app.chat.message as message
+import spit_app.chat.render as render
+
+async def undo(self) -> None:
+    if self.undo_index >= 0:
+        operation, umessage, index = self.undo[self.undo_index]
+        if operation == "remove":
+            self.messages.append(umessage.copy())
+            await render.message(self, umessage, True)
+        if operation == "append":
+            del self.messages[-1]
+            await message.remove_last_turn(self)
+        if operation == "change":
+            temp_umessage = self.messages[index].copy()
+            self.messages[index] = umessage.copy()
+            self.undo[self.undo_index] = [operation, temp_umessage.copy(), index]
+            self.edit = True
+            self.edit_container = self.query_one("#chat-view").children[index]
+            await render.message(self, umessage, True)
+            self.edit = False
+        self.write_chat_history()
+        self.undo_index-=1
+
+async def redo(self) -> None:
+    if self.undo_index < len(self.undo)-1:
+        self.undo_index+=1
+        operation, umessage, index = self.undo[self.undo_index]
+        if operation == "remove":
+            del self.messages[-1]
+            await message.remove_last_turn(self)
+        if operation == "append":
+            self.messages.append(umessage.copy())
+            await render.message(self, umessage, True)
+        if operation == "change":
+            temp_umessage = self.messages[index].copy()
+            self.messages[index] = umessage.copy()
+            self.undo[self.undo_index] = [operation, temp_umessage.copy(), index]
+            self.edit = True
+            self.edit_container = self.query_one("#chat-view").children[index]
+            await render_message(self, umessage, True)
+            self.edit = False
+        self.write_chat_history()
+
+def append_undo(self, operation: str, umessage: dict, index: int = -1) -> None:
+    while len(self.undo)-1 > self.undo_index:
+        del self.undo[-1]
+    while len(self.undo) > 100:
+        del self.undo[0]
+    self.undo.append([operation, umessage.copy(), index])
+    self.undo_index=len(self.undo)-1
