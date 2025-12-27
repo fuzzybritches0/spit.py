dee9509f86c9b5fceed763c9f4e4e08905b17209
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 18:58:38 +0000

    app: refactor for multi-chat view
    - Make the chat application a sub-application. Remove it from the main
      application.
    - Add copyright and license vars.

--- spit_app/app.py
+++ spit_app/app.py
@@ -1,52 +1,45 @@
 # SPDX-License-Identifier: GPL-2.0
-import json
 from textual.app import App, ComposeResult
-from textual.containers import VerticalScroll
-from textual.widgets import Footer, Header, TextArea
-import spit_app.utils as utils
+from textual.containers import Vertical, Horizontal
+from textual.widgets import Header, Footer
 from spit_app.settings import Settings
 from spit_app.actions import ActionsMixIn
 from spit_app.actions import bindings
 from spit_app.handlers import HandlersMixIn
+from spit_app.side_panel.side_panel import SidePanel
 
-class SpitApp(HandlersMixIn, ActionsMixIn, App):
+class SpitApp(ActionsMixIn, HandlersMixIn, App):
     NAME = "spit.py"
     VERSION = "0.1"
-    CSS_PATH = './styles/main.css'
+    TITLE = f"{NAME} v{VERSION}"
+    COPYRIGHT = "github.com/fuzzybritches0"
+    LICENSE = "GPL-2.0"
+    CSS_PATH = './styles/app.css'
     BINDINGS = bindings
 
     def __init__(self):
         super().__init__()
         #self.applog("LOG START:")
-        self.settings = Settings()
+        self.settings = Settings(self)
         self.settings.load()
-        if self.settings.theme:
-            self.theme = self.settings.theme
-        self.title_update()
-        utils.load_messages(self)
-        utils.load_system_prompt(self)
-        self.work = None
-        self.edit = False
-        self.focused_message = None
-        self.text_area_was_empty = True
         self.watch(self.app, "theme", self.on_theme_changed, init=False)
 
     def applog(self, text: str) -> None:
         with open("./log.txt", "a") as file:
             file.write(repr(text) + "\n")
 
-    def title_update(self) -> None:
-        active = self.settings.active_endpoint
-        name = "None"
-        if active:
-            name = self.settings.endpoints[active]["values"]["name"]
-        self.title = f"{self.NAME} v{self.VERSION} - Chat - {name}"
-
     def compose(self) -> ComposeResult:
         yield Header()
-        self.chat_view = VerticalScroll(id="chat-view")
-        yield self.chat_view
-        self.chat_view.anchor()
-        self.text_area = TextArea(id="text-area", tab_behavior="indent")
-        yield self.text_area
+        with Horizontal(id="app"):
+            yield SidePanel()
+            yield Vertical(id="main")
         yield Footer()
+
+    def focus_first(self, index: int) -> None:
+        child = self.query_one("#main").children[index]
+        if child.id.startswith("chat-"):
+            child.children[0].focus()
+        elif child.children[0].id == "option-list":
+            child.children[0].focus()
+        else:
+            child.children[1].focus()
