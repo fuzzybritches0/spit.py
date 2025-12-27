a6b98badb2dbef808b91d3aa2d3e7f10bc86df1e
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 20:00:54 +0000

    manage: prompts: refactor
    Remove unnecessary action methods. We no longer need to dismiss the
    sub-application. Once loaded it remains loaded.

--- spit_app/manage/prompt/actions.py
+++ spit_app/manage/prompt/actions.py
@@ -1,21 +1,26 @@
 class ActionsMixIn:
     async def action_delete(self) -> None:
         self.settings.delete_system_prompt(self.cur_system_prompt)
-        self.app.title_update()
-        await self.clean_dyn_container()
+        await self.remove_children()
         await self.select_main_screen()
 
     async def action_save(self) -> None:
         if self.valid_values_edit():
             self.store_values()
             self.settings.save_system_prompts()
-            self.app.title_update()
-            await self.clean_dyn_container()
+            await self.remove_children()
             await self.select_main_screen()
 
-    async def action_dismiss(self) -> None:
-        if self.dyn_container.children[0].id == "select-main":
-            self.dismiss()
-        else:
-            await self.clean_dyn_container()
-            await self.select_main_screen()
+    async def action_cancel(self) -> None:
+        await self.remove_children()
+        await self.select_main_screen()
+
+    def is_edit_prompt(self) -> bool:
+        if self.children and self.children[0].id == "edit-prompt":
+            return True
+        return False
+
+    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
+        if action == "cancel" or action == "save" or action == "delete":
+            return self.is_edit_prompt()
+        return True
