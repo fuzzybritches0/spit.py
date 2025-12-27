7d5a954948e6eaf1fa4c59051641efc3fba6a0da
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 19:35:38 +0000

    manage: endpoint: refactor
    Move from main.py to endpoint.py and refactor for the new side-panel
    functionality.

--- spit_app/manage/endpoint/endpoint.py
+++ spit_app/manage/endpoint/endpoint.py
@@ -1,20 +1,25 @@
 from textual.widgets import Select
+from textual.containers import VerticalScroll
 from .actions import ActionsMixIn
 from .handlers import HandlersMixIn
 from .screens import ScreensMixIn
 from .validation import ValidationMixIn
-from spit_app.manage.common import CommonMixIn
+from spit_app.manage.validation import Validation
 
-class Endpoints(ActionsMixIn, HandlersMixIn,
-                ScreensMixIn, ValidationMixIn,
-                CommonMixIn):
-    CSS_PATH = "../../styles/manage.css"
+class Endpoints(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, VerticalScroll):
     BINDINGS = [
         ("ctrl+enter", "save", "Save"),
         ("ctrl+r", "delete", "Delete"),
         ("ctrl+s", "set_active", "Set active"),
-        ("escape", "dismiss", "Cancel")
+        ("escape", "cancel", "Cancel")
     ]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.settings = self.app.settings
+        self.id = "manage-endpoints"
+        self.classes = "manage"
+        self.val = Validation(self)
 
     def store_values(self) -> None:
         for setting, stype, desc, array in self.settings.endpoints[self.cur_endpoint]["custom"]:
