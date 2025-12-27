c78589d5f625724fb42a92d785a15716a0e2e06b
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 20:33:14 +0000

    manage: chat: handlers: add handlers

---
+++ spit_app/manage/chat/handlers.py
@@ -0,0 +1,27 @@
+import os
+import json
+from datetime import datetime
+from textual.widgets import Button, OptionList
+from textual.widgets.option_list import Option
+
+class HandlersMixIn:
+    async def on_mount(self) -> None:
+        await self.select_main_screen()
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancel":
+            await self.remove_children()
+            await self.select_main_screen()
+        elif event.button.id == "delete":
+            await self.action_delete()
+        elif event.button.id == "save":
+            await self.action_save()
+
+    async def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
+        if event.option.id == "select-new-chat":
+            await self.remove_children()
+            await self.edit_chat()
+        else:
+            self.cur_chat = str(event.option.id)
+            await self.remove_children()
+            await self.edit_chat(self.cur_chat)
