5dab46d5c7f83a0e0e93109c1c1e0088710dc225
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 19:37:39 +0000

    manage: endpoint: refactor screens
    Remove unnecessary containers and adjust to the new side-panel
    functionality. All manage sub-applications reside in the #main
    container and stay there once loaded.

--- spit_app/manage/endpoint/screens.py
+++ spit_app/manage/endpoint/screens.py
@@ -2,42 +2,38 @@
 from textual.validation import Function
 from textual.widgets import Input, TextArea, Switch, Label, Button, OptionList, Select, Rule
 from textual.widgets.option_list import Option
-from textual.containers import VerticalScroll, Horizontal
+from textual.containers import Horizontal
 
 class ScreensMixIn:
     async def mount_custom_setting_form(self, stype) -> None:
-        vscroll = self.dyn_container.children[0]
-        await vscroll.mount(Label(f"Setting for {stype} value:"))
+        await self.mount(Label(f"Setting for {stype} value:"))
         Validators = [ Function(self.val.is_unique_custom), Function(self.val.is_valid_setting) ]
-        await vscroll.mount(Input(id="new-setting",
+        await self.mount(Input(id="new-setting",
                                        validators=Validators,
                                        restrict=r"[0-9a-z_.]*",
                                        valid_empty=False,
                                        max_length=128))
-        await vscroll.mount(Label(f"Description for {stype} value:"))
-        await vscroll.mount(Input(id="new-description", validators=[Function(self.val.is_not_empty)]))
+        await self.mount(Label(f"Description for {stype} value:"))
+        await self.mount(Input(id="new-description", validators=[Function(self.val.is_not_empty)]))
         if stype == "Select" or stype == "Select_no_default":
-            await vscroll.mount(Label("Select values (separate with ','):"))
+            await self.mount(Label("Select values (separate with ','):"))
             Validators = [ Function(self.val.is_valid_selection) ]
-            await vscroll.mount(Input(id="new-select-values", validators=Validators,
+            await self.mount(Input(id="new-select-values", validators=Validators,
                                            restrict=r"[0-9a-z_, ]*"))
-        await vscroll.mount(Button("Add", id=f"button-add-setting"))
+        await self.mount(Button("Add", id=f"button-add-setting"))
 
     async def mount_settings_add_custom(self, stype) -> None:
-        vscroll = self.dyn_container.children[0]
-        while not vscroll.children[-1].id == "custom-setting-select-add":
-            await vscroll.children[-1].remove()
+        while not self.children[-1].id == "custom-setting-select-add":
+            await self.children[-1].remove()
         if not stype == Select.BLANK:
             await self.mount_custom_setting_form(stype)
 
     async def edit_endpoint_add_custom(self) -> None:
-        vscroll = self.dyn_container.children[0]
         types = [ "Integer", "Float", "Boolean", "String", "Text", "Select", "Select_no_default" ]
-        await vscroll.mount(Label("Add custom setting:"))
-        await vscroll.mount(Select.from_values(types, id="custom-setting-select-add", allow_blank=False))
+        await self.mount(Label("Add custom setting:"))
+        await self.mount(Select.from_values(types, id="custom-setting-select-add", allow_blank=False))
 
     async def edit_endpoint_remove_custom(self) -> None:
-        vscroll = self.dyn_container.children[0]
         names = []
         for setting, *others in self.settings.endpoints[self.cur_endpoint]["custom"]:
             if (not setting == "name" and
@@ -45,15 +41,14 @@ class ScreensMixIn:
                 not setting == "key" and
                 not setting == "reasoning_key"):
                 names.append(setting)
-        await vscroll.mount(Label("Remove custom setting:"))
-        await vscroll.mount(Select.from_values(names, id="custom-setting-select-remove", allow_blank=False))
-        await vscroll.mount(Button("Remove", id=f"button-remove-setting"))
+        await self.mount(Label("Remove custom setting:"))
+        await self.mount(Select.from_values(names, id="custom-setting-select-remove", allow_blank=False))
+        await self.mount(Button("Remove", id="button-remove-setting"))
 
     async def edit_endpoint(self) -> None:
-        vscroll = self.dyn_container.children[0]
         for setting, stype, desc, amore in self.settings.endpoints[self.cur_endpoint]["custom"]:
             id = setting.replace(".", "-")
-            await vscroll.mount(Label(f"{desc}: ({stype})"))
+            await self.mount(Label(f"{desc}: ({stype})"))
             value = None
             if setting in self.settings.endpoints[self.cur_endpoint]["values"]:
                 value = self.settings.endpoints[self.cur_endpoint]["values"][setting]
@@ -75,44 +70,38 @@ class ScreensMixIn:
             if stype == "Select":
                 if not value:
                     value = Select.BLANK
-                await vscroll.mount(Select.from_values(amore, id=id, value=value, prompt="Default"))
+                await self.mount(Select.from_values(amore, id=id, value=value, prompt="Default"))
             elif stype == "Select_no_default":
                 if not value:
                     value = amore[0]
-                await vscroll.mount(Select.from_values(amore, id=id, value=value, allow_blank=False))
+                await self.mount(Select.from_values(amore, id=id, value=value, allow_blank=False))
             elif stype == "Boolean":
-                await vscroll.mount(Switch(id=id, value=value))
+                await self.mount(Switch(id=id, value=value))
             elif stype == "Text":
-                await vscroll.mount(TextArea(value, id=id))
+                await self.mount(TextArea(value, id=id, classes="text-area"))
             else:
-                await vscroll.mount(Input(type=vtype, validators=Validators,
+                await self.mount(Input(type=vtype, validators=Validators,
                                           id=f"{setting}", value=value))
 
     async def edit_endpoint_screen(self) -> None:
-        vscroll = VerticalScroll(id="edit-endpoint")
-        await self.dyn_container.mount(vscroll)
         await self.edit_endpoint()
-        await vscroll.mount(Rule())
+        await self.mount(Horizontal(
+                Button("Save", id="save"),
+                Button("Delete", id="delete"),
+                Button("Set Active", id="set-active"),
+                Button("Cancel", id="cancel"),
+                id="save-delete-cancel"
+        ))
+        await self.mount(Rule())
         await self.edit_endpoint_remove_custom()
-        await vscroll.mount(Rule())
+        await self.mount(Rule())
         await self.edit_endpoint_add_custom()
-        horiz = Horizontal(id="save-delete-cancel")
-        await self.dyn_container.mount(horiz)
-        await horiz.mount(Button("Save", id="save"))
-        await horiz.mount(Button("Delete", id="delete"))
-        await horiz.mount(Button("Set Active", id="set-active"))
-        await horiz.mount(Button("Cancel", id="cancel"))
-        vscroll.children[1].focus()
+        self.children[1].focus()
 
     async def select_main_screen(self) -> None:
-        vscroll = VerticalScroll(id="select-main")
-        await self.dyn_container.mount(vscroll)
         Options = [ Option("\nCreate new endpoint\n", id="select-new-endpoint") ]
         for endpoint in self.settings.endpoints.keys():
             name=self.settings.endpoints[endpoint]["values"]["name"]
             Options.append(Option(f"\nEdit: {name}\n", id=f"select-endpoint-{endpoint}"))
-        await vscroll.mount(OptionList(*Options, id="option-list"))
-        horiz = Horizontal(id="cancel-container")
-        await self.dyn_container.mount(horiz)
-        await horiz.mount(Button("Cancel", id="cancel"))
-        vscroll.children[0].focus()
+        await self.mount(OptionList(*Options, id="option-list"))
+        self.children[0].focus()
