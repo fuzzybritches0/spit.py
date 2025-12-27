cd4bcc0148e1463e2f5782c5de446b669d696129
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 19:13:33 +0000

    app: handlers: remove chat-app related handlers
    Remove all chat-app related handlers and add a method that loads the
    last active chat-app.

--- spit_app/handlers.py
+++ spit_app/handlers.py
@@ -1,58 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0
-from textual import events
-import spit_app.message as message
-import spit_app.latex_math as lm
-from spit_app.overlays.loading_screen import LoadingScreen
+import os
+from spit_app.chat.chat import Chat
 
 class HandlersMixIn:
-    async def on_ready(self) -> None:
-        self.loading_screen = LoadingScreen()
-        await self.push_screen(self.loading_screen)
-        await message.render_messages(self)
-        self.loading_screen.dismiss()
-        if len(self.chat_view.children) == 0:
-            self.text_area.focus()
-
-    def on_worker_state_changed(self) -> None:
-        self.refresh_bindings()
-
-    def on_descendant_focus(self, event: events.DescendantFocus) -> None:
-        if event.control is self.chat_view:
-            if self.focused_message:
-                self.focused_message.focus(scroll_visible=False)
-        elif event.control.id and ("-listing-" in event.control.id or
-                                   "message-id-" in event.control.id):
-            self.focused_message = event.control
-
-    def on_text_area_changed(self) -> None:
-        if self.text_area.text:
-            if self.text_area_was_empty:
-                self.refresh_bindings()
-                self.text_area_was_empty = False
-        else:
-            if not self.text_area_was_empty:
-                self.refresh_bindings()
-                self.text_area_was_empty = True
-
     async def on_theme_changed(self, old_value:str, new_value:str) -> None:
-        cont_count=0
-        self.app.settings.theme = self.theme
-        self.app.settings.save()
-        for message_container in self.latex_listings:
-            latex_count=0
-            for latex in message_container:
-                id="latex-listing-"+str(cont_count)+"-"+str(latex_count)
-                container=self.query_one("#"+id)
-                color = container.parent.styles.color.css
-                background = container.parent.styles.background.css
-                sequence = self.latex_listings[cont_count][latex_count]
-                if sequence.startswith("$") and not sequence.startswith("$$"):
-                    sequence = sequence[1:-1]
-                else:
-                    sequence = sequence[2:-2]
-                latex_image = lm.latex_math(self, sequence.strip(" \n"), color, background)
-                if latex_image:
-                    await container.remove_children()
-                    await container.mount(latex_image)
-                latex_count+=1
-            cont_count+=1
+        self.settings.theme = self.theme
+        self.settings.save()
+
+    async def on_ready(self) -> None:
+        if self.settings.active_chat:
+            active = self.settings.active_chat
+            file_name = active + ".json"
+            if os.path.exists(self.settings.data_path / file_name):
+                index = self.query_one("#side-panel").get_option_index(active)
+                self.query_one("#side-panel").highlighted = index
+                await self.query_one("#main").mount(Chat(active))
+            else:
+                self.settings.active_chat = None
+                self.settings.save()
