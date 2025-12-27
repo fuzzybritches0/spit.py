9f5a61f120fa27cf0d7e001fa533ad8f60aa1a2b
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-27 20:03:01 +0000

    manage: prompt: refactor handlers
    Refactor handlers to align with the new side-panel functionality.

--- spit_app/manage/prompt/handlers.py
+++ spit_app/manage/prompt/handlers.py
@@ -1,9 +1,13 @@
 from textual.widgets import Button, OptionList
 
 class HandlersMixIn:
+    async def on_mount(self) -> None:
+        await self.select_main_screen()
+
     async def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "cancel":
-            await self.action_dismiss()
+            await self.remove_children()
+            await self.select_main_screen()
         elif event.button.id == "delete":
             await self.action_delete()
         elif event.button.id == "save":
@@ -12,9 +16,9 @@ class HandlersMixIn:
     async def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
         if event.option.id == "select-new-prompt":
             self.cur_system_prompt = self.settings.new_system_prompt()
-            await self.clean_dyn_container()
+            await self.remove_children()
             await self.edit_prompt_screen()
         else:
             self.cur_system_prompt = str(event.option.id[14:])
-            await self.clean_dyn_container()
+            await self.remove_children()
             await self.edit_prompt_screen()
