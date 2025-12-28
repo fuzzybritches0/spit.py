96ff28fec72257de561b6da519069d50e78c8eaf
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-28 00:29:11 +0000

    manage: endpoint: remove manage chat on save
    Remove the manage chat sub-application if the user modifies or creates a
    new endpoint so when selected, manage chat will re-render and be
    up-to-date.

--- spit_app/manage/endpoint/actions.py
+++ spit_app/manage/endpoint/actions.py
@@ -11,6 +11,10 @@ class ActionsMixIn:
         if self.valid_values_edit():
             self.store_values()
             self.settings.save()
+            try:
+                await self.app.query_one("#main").query_one("#manage-chats").remove()
+            except:
+                pass
             await self.remove_children()
             await self.select_main_screen()
 
