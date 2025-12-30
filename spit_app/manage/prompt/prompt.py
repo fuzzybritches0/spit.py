9dce85fdcd005c1834a836878b86b924f2524cc4
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-30 09:17:41 +0000

    manage: prompt: chat: move methods from settings
    Move prompt specific methods from settings to the maim module and store
    values while editing in self.uuid and self.prompt.

--- spit_app/manage/prompt/prompt.py
+++ spit_app/manage/prompt/prompt.py
@@ -1,9 +1,10 @@
+from uuid import uuid4
+from copy import deepcopy
 from textual.containers import Vertical
 from .actions import ActionsMixIn
 from .handlers import HandlersMixIn
 from .screens import ScreensMixIn
 from .validation import ValidationMixIn
-from spit_app.manage.validation import Validation
 
 class Prompts(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Vertical):
     BINDINGS = [
@@ -18,12 +19,32 @@ class Prompts(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Vertic
         self.id = "manage-prompts"
         self.classes = "manage"
         self.new_prompt = False
-        self.val = Validation(self)
+
+    def new(self) -> None:
+        self.new_prompt = True
+        self.uuid = str(uuid4())
+        self.prompt = {
+                "name": self.uuid,
+                "text": "You are a helpful AI assistant."
+        }
+
+    def load(self, uuid: str) -> None:
+        self.new_prompt = False
+        self.uuid = uuid
+        self.prompt = deepcopy(self.settings.prompts[uuid])
 
     def store_values(self) -> None:
         name = self.query_one("#name").value
         text = self.query_one("#text").text
-        self.settings.prompts[self.cur_prompt] = {
+        self.prompt = {
                 "name": name,
                 "text": text
         }
+
+    def save(self) -> None:
+        self.settings.prompts[self.uuid] = deepcopy(self.prompt)
+        self.settings.save_prompts()
+
+    def delete(self) -> None:
+        del self.settings.prompts[self.uuid]
+        self.settings.save_prompts()
