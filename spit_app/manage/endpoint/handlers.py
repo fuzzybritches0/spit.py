2e65fbdc7feeb1de358bf7d3615858af2351683d
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-30 09:11:46 +0000

    manage: endpoint: handlers: adjust method calling

--- spit_app/manage/endpoint/handlers.py
+++ spit_app/manage/endpoint/handlers.py
@@ -23,12 +23,10 @@ class HandlersMixIn:
 
     async def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
         if event.option.id == "select-new-endpoint":
-            self.new_endpoint = True
-            self.cur_endpoint = self.settings.new()
+            self.new()
             await self.remove_children()
             await self.edit_endpoint_screen()
         else:
-            self.new_endpoint = False
-            self.cur_endpoint = str(event.option.id[16:])
+            self.load(str(event.option.id[16:]))
             await self.remove_children()
             await self.edit_endpoint_screen()
