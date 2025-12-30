4fd108e9913891f05e6af51548671a112ff22ba6
Kurt Manucredo <fuzzybritches0@gmail.com>
2025-12-30 09:06:26 +0000

    manage: endpoint: actions: adjust method calling
    Adjust calling mehtods from main module. Also, revert a former change
    where we removed Select.BLANK as a selection. Should the user remove all
    removable custom settings, the application will crash, since Select
    options must not be empty.

--- spit_app/manage/endpoint/actions.py
+++ spit_app/manage/endpoint/actions.py
@@ -1,15 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0
+from textual.widgets import Select
 
 class ActionsMixIn:
     async def action_delete(self) -> None:
-        self.settings.delete_endpoint(self.cur_endpoint)
+        self.delete()
         await self.remove_children()
         await self.select_main_screen()
 
     async def action_save(self) -> None:
         if self.valid_values_edit():
             self.store_values()
-            self.settings.save()
+            self.save()
             try:
                 await self.app.query_one("#main").query_one("#manage-chats").remove()
             except:
@@ -27,20 +28,20 @@ class ActionsMixIn:
             array = value.split(",")
             for el in array:
                 sarray.append(el.strip())
-        self.settings.add_custom_setting(self.cur_endpoint, setting, stype, desc, sarray)
+        self.add_custom_setting(setting, stype, desc, sarray)
         self.query_one("#custom-setting-select-remove").set_options(self.custom_options())
         await self.mount_setting(setting, stype, desc, sarray, True)
 
     async def action_remove_setting(self) -> None:
         remove = self.query_one("#custom-setting-select-remove").value
-        self.settings.remove_custom_setting(self.cur_endpoint, remove)
-        self.query_one("#custom-setting-select-remove").set_options(self.custom_options())
-        remove = remove.replace(".", "-")
-        await self.query_one(f"#{remove}").remove()
-        await self.query_one(f"#label-{remove}").remove()
+        if not remove == Select.BLANK:
+            self.remove_custom_setting(remove)
+            self.query_one("#custom-setting-select-remove").set_options(self.custom_options())
+            remove = remove.replace(".", "-")
+            await self.query_one(f"#{remove}").remove()
+            await self.query_one(f"#label-{remove}").remove()
 
     async def action_cancel(self) -> None:
-        self.settings.load()
         await self.remove_children()
         await self.select_main_screen()
 
