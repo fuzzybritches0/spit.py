9016fb46050cb069454c48798b2bf83fd3633f50
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-01 08:34:18 +0000

    tools: add weather tool call example
    Add bogus weather tool call example.

---
+++ spit_app/tools/get_current_weather.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+
+desc = {
+    "type": "function",
+    "function": {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA"
+                },
+                "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]}
+            },
+            "required": ["location"]
+        }
+    }
+} 
+    
+def call(app, arguments: dict, chat_id) -> dict:
+    return '{"unit":"celsius","temperature":3}'
