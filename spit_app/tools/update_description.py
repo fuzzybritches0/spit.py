068db63e2bc3979b6460b77b030e9e1fa65962d4
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-03 10:00:45 +0000

    tools: update_description: fix updating
    Properly update the time stamp in #side-panel and force reloading of
    manage chats to to up-to-date.

--- spit_app/tools/update_description.py
+++ spit_app/tools/update_description.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
+from datetime import datetime
 
 desc = {
     "type": "function",
@@ -21,7 +22,12 @@ desc = {
 async def call_async(app, arguments: dict, chat_id) -> str:
     chat = app.query_one(f"#{chat_id}")
     desc = arguments["description"]
+    ctime = datetime.fromtimestamp(chat.chat_ctime)
     chat.chat_desc = desc
     chat.write_chat_history
-    app.query_one("#side-panel").replace_option_prompt(chat_id, f"\n{desc}\n{chat.chat_ctime}\n")
+    app.query_one("#side-panel").replace_option_prompt(chat_id, f"\n{desc}\n{ctime}\n")
+    try:
+        await app.query_one("#main").query_one("#manage-chats").remove()
+    except:
+        pass
     return "chat description set."
