d3517d71bc6bdd90f4e033f78af8217132ddff4c
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-06 17:04:45 +0000

    manage: endpoint: validation: prepare for reuse
    Refactor code so we will be able to reuse it later.

--- spit_app/manage/endpoint/validation.py
+++ spit_app/manage/endpoint/validation.py
@@ -1,60 +1,70 @@
 # SPDX-License-Identifier: GPL-2.0
 
 class ValidationMixIn:
-    def valid_values_add(self) -> bool:
-        if not self.query_one("#new-setting").is_valid:
+    def is_valid(self, stype: str, usign: bool, value) -> bool:
+        stypes = { "integer": int, "float": float }
+        if not isinstance(value, str) or not value.strip():
             return False
-        if not self.query_one("#new-description").is_valid:
+        try:
+            stypes[stype](value)
+            if not usign:
+                if stypes[stype](value) < 0:
+                    return False
+            return True
+        except ValueError:
             return False
-        stype = self.query_one("#custom-setting-select-add").value
-        if stype == "Select" or stype == "Select_no_default":
-            if not self.query_one("#new-select-values").is_valid:
+
+    def valid_setting_name(self, value: str) -> bool:
+        if not value:
+            return False
+        for endpoint in self.app.settings.endpoints.keys():
+            if (not endpoint == self.uuid and
+                    self.app.settings.endpoints[endpoint]["name"]["value"].strip() == value.strip()):
                 return False
         return True
+
+    def valid_url(self, value) -> bool:
+        if value.startswith("http://"):
+            return True
+        if value.startswith("https://"):
+            return True
+        return False
+
+    def valid_float(self, value: str) -> bool:
+        return self.is_valid("float", True, value)
+
+    def valid_ufloat(self, value: str) -> bool:
+        return self.is_valid("float", False, value)
+
+    def valid_integer(self, value: str) -> bool:
+        return self.is_valid("integer", True, value)
+
+    def valid_uinteger(self, value: str) -> bool:
+        return self.is_valid("integer", False, value)
 
     def is_not_empty(self, value) -> bool:
         if value:
             return True
         return False
 
-    def valid_values_edit(self) -> bool:
-        for setting in self.endpoint.keys():
-            stype = self.endpoint[setting]["stype"]
-            id = setting.replace(".", "-")
-            if (not stype == "boolean" and not stype == "select" and
-                not stype == "select_no_default" and not stype == "text"):
-                if not self.query_one(f"#{id}").is_valid:
-                    return False
-        return True
-
-    def is_url(self, value) -> bool:
-        if value.startswith("http://"):
-            return True
-        if value.startswith("https://"):
-            return True
-        return False
-    
-    def is_unique_name(self, value: str) -> bool:
-        if not value:
-            return False
-        for endpoint in self.app.settings.endpoints.keys():
-            if (not endpoint == self.uuid and
-                    self.app.settings.endpoints[endpoint]["name"]["value"] == value):
-                return False
-        return True
-
     def is_unique_custom(self, value: str) -> bool:
         if not value:
             return False
         same = 0
-        for name in self.app.settings.endpoints[self.uuid].keys():
-            if name == value:
+        for name in self.endpoint.keys():
+            if name.strip() == value.strip():
                 return False
         return True
 
     def is_valid_setting(self, value: str) -> bool:
         value = value.strip()
         if not value:
+            return False
+        if " " in value:
+            return False
+        if any(ch.isupper() for ch in value):
+            return False
+        if value[0].isdecimal():
             return False
         if value.startswith("_") or value.endswith("_"):
             return False
