a761f4bd482f5b35d2f137addb877db6613e6613
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-08 14:41:14 +0000

    manage: endpoint: de-duplicate code: (3/5)
    De-duplicate handlers methods in endpoint.

--- spit_app/manage/endpoint/handlers.py
+++ spit_app/manage/endpoint/handlers.py
@@ -1,34 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0
-from textual.widgets import Button, OptionList, Select
+from textual.widgets import Button, Select
 
 class HandlersMixIn:
-    async def on_mount(self) -> None:
-        await self.select_main_screen()
-
     async def on_select_changed(self, event: Select.Changed) -> None:
         if event.control.id == "custom-setting-select-add":
             await self.mount_settings_add_custom(event.value)
 
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "cancel":
-            await self.action_cancel()
-        elif event.button.id == "delete":
-            await self.action_delete()
-        elif event.button.id == "duplicate":
-            self.action_duplicate()
-        elif event.button.id == "save":
-            await self.action_save()
-        elif event.button.id == "button-remove-setting":
+    async def on_button_pressed_extra(self, event: Button.Pressed) -> None:
+        if event.button.id == "button-remove-setting":
             await self.action_remove_setting()
         elif event.button.id == "button-add-setting":
             await self.action_add_setting()
-
-    async def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
-        if event.option.id == "select-new-manage":
-            self.new()
-            await self.remove_children()
-            await self.edit_manage_screen()
-        else: 
-            self.load(event.option.id.split("-",2)[2])
-            await self.remove_children()
-            await self.edit_manage_screen()
