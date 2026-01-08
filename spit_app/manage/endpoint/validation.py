0a295a713a0bed4a18aad93a9a9d27deb881eeb5
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-08 14:42:40 +0000

    manage: endpoint: de-duplicate code: (5/5)
    De-duplicate validation methods in endpoint.

--- spit_app/manage/endpoint/validation.py
+++ spit_app/manage/endpoint/validation.py
@@ -1,21 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
-import string
 
 class ValidationMixIn:
-    def is_valid(self, stype: str, usign: bool, value: str) -> bool:
-        stypes = { "integer": int, "float": float }
-        if value:
-            try:
-                stypes[stype](value)
-                if not usign:
-                    if stypes[stype](value) < 0:
-                        return False
-                return True
-            except ValueError:
-                return False
-        else:
-            return True
-
     def valid_setting_name(self, value: str) -> bool:
         if not value:
             return False
@@ -25,30 +10,6 @@ class ValidationMixIn:
                 return False
         return True
 
-    def valid_url(self, value) -> bool:
-        if value.startswith("http://"):
-            return True
-        if value.startswith("https://"):
-            return True
-        return False
-
-    def valid_float(self, value: str) -> bool:
-        return self.is_valid("float", True, value)
-
-    def valid_ufloat(self, value: str) -> bool:
-        return self.is_valid("float", False, value)
-
-    def valid_integer(self, value: str) -> bool:
-        return self.is_valid("integer", True, value)
-
-    def valid_uinteger(self, value: str) -> bool:
-        return self.is_valid("integer", False, value)
-
-    def is_not_empty(self, value) -> bool:
-        if value:
-            return True
-        return False
-
     def is_unique_custom(self, value: str) -> bool:
         if value:
             for name in self.manage.keys():
@@ -57,51 +18,6 @@ class ValidationMixIn:
             return True
         else:
             return True
-
-    def valid_chars(self, value: str) -> bool:
-        chars=string.ascii_lowercase + string.digits + "_.-"
-        for char in value:
-            if not char in chars:
-                return False
-        return True
-
-    def is_valid_setting(self, value: str) -> bool:
-        if value:
-            if not self.valid_chars(value):
-                return False
-            if value[0].isdecimal():
-                return False
-            if value.startswith("_") or value.endswith("_"):
-                return False
-            if value.startswith(".") or value.endswith("."):
-                return False
-            return True
-        else:
-            return True
-
-    def is_valid_selection(self, value: str) -> bool:
-        if value:
-            values = value.split(",")
-            for value in values:
-                if not value:
-                    return False
-                if not self.is_valid_setting(value.strip()):
-                    return False
-            return True
-        else:
-            return False
-
-    def validate_values_edit(self) -> bool:
-        for setting in self.manage.keys():
-            stype = self.manage[setting]["stype"]
-            id = setting.replace(".", "-")
-            if (not stype == "boolean" and not stype == "select" and
-                not stype == "select_no_default" and not stype == "text"):
-                inp = self.query_one(f"#{id}")
-                inp.validate(inp.value)
-                if not inp.is_valid:
-                    return False
-        return True
 
     def validate_add_setting(self) -> bool:
         setting = self.query_one("#new-setting")
