1c7c92382f38910660b720238bf547e398266bc2
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-08 10:16:09 +0000

    mange: endpoint: 2/5: prepare for de-duplication
    Give all objects and variables in endpoint a common name so we can
    de-duplicate code.

--- spit_app/manage/endpoint/endpoint.py
+++ spit_app/manage/endpoint/endpoint.py
@@ -17,20 +17,21 @@ class Endpoints(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Vert
 
     def __init__(self) -> None:
         super().__init__()
-        self.settings = self.app.settings
         self.id = "manage-endpoints"
         self.classes = "manage"
-        self.new_endpoint = False
+        self.managed = self.app.settings.endpoints
+        self.save_managed = self.app.settings.save_endpoints
+        self.new_manage = False
 
     def duplicate(self) -> None:
-        self.new_endpoint = True
+        self.new_manage = True
         self.uuid = str(uuid4())
         self.query_one("#name").value = self.uuid
 
     def new(self) -> None:
-        self.new_endpoint = True
+        self.new_manage = True
         self.uuid = str(uuid4())
-        self.endpoint = {
+        self.manage = {
             "name": { "stype": "string", "empty": False, "desc": "Name", "value": self.uuid },
             "endpoint_url": { "stype": "url", "empty": False, "desc": "Endpoint URL",
                             "value": "http://127.0.0.1:8080" },
@@ -41,17 +42,17 @@ class Endpoints(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Vert
             "top_p": { "stype": "float", "desc": "TOP-P" },
             "min_p": { "stype": "float", "desc": "MIN-P" },
             "top_k": { "stype": "float", "desc": "TOP-K" }
-        }
+    }
 
     def load(self, uuid: str) -> None:
-        self.new_endpoint = False
+        self.new_manage = False
         self.uuid = uuid
-        self.endpoint = deepcopy(self.settings.endpoints[uuid])
+        self.manage = deepcopy(self.managed[uuid])
 
     def store_values(self) -> None:
-        for setting in self.endpoint.keys():
+        for setting in self.manage.keys():
             id = setting.replace(".", "-")
-            if self.endpoint[setting]["stype"] == "text":
+            if self.manage[setting]["stype"] == "text":
                 newvalue = self.query_one(f"#{id}").text
             else:
                 newvalue = self.query_one(f"#{id}").value
@@ -60,28 +61,28 @@ class Endpoints(ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Vert
             self.store_value(setting, newvalue)
 
     def store_value(self, setting: str, value: str|bool) -> None:
-        stype = self.endpoint[setting]["stype"]
+        stype = self.manage[setting]["stype"]
         if (stype == "float" or stype == "ufloat") and value:
             value = float(value)
         elif (stype == "integer" or stype == "uinteger") and value:
             value = int(value)
         else:
             value = value.strip()
-        self.endpoint[setting]["value"] = value
+        self.manage[setting]["value"] = value
 
     def save(self) -> None:
-        self.settings.endpoints[self.uuid] = deepcopy(self.endpoint)
-        self.settings.save_endpoints()
+        self.managed[self.uuid] = deepcopy(self.manage)
+        self.save_managed()
 
     def delete(self) -> None:
-        del self.settings.endpoints[self.uuid]
-        self.settings.save_endpoints()
+        del self.managed[self.uuid]
+        self.save_managed()
 
     def add_custom_setting(self, setting: str, stype: str, desc: str, sarray: list = []) -> None:
         if not sarray:
-            self.endpoint[setting] = { "stype": stype, "desc": desc }
+            self.manage[setting] = { "stype": stype, "desc": desc }
         else:
-            self.endpoint[setting] = { "stype": stype, "desc": desc , "options": sarray}
+            self.manage[setting] = { "stype": stype, "desc": desc , "options": sarray}
 
     def remove_custom_setting(self, rsetting: str) -> None:
-        del self.endpoint[rsetting]
+        del self.manage[rsetting]
