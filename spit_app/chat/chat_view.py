2f6da4ae695203afba4b82314e05b74386cb5db9
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-17 23:49:55 +0000

    chat: chat_view: refactor
    Move all relevant code here and refactor chat_view.

---
+++ spit_app/chat/chat_view.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: GPL-2.0
+from textual.containers import VerticalScroll
+from .message.message import Message
+from .work import Work
+
+class ChatView(VerticalScroll):
+    BINDINGS = [
+        ("ctrl+enter", "continue", "Continue"),
+        ("u", "undo", "Undo"),
+        ("r", "redo", "Redo")
+    ]
+
+    def __init__(self, chat) -> None:
+        super().__init__()
+        self.anchor()
+        self.chat = chat
+        self.messages = self.chat.messages
+        self.id = "chat-view"
+        self.focused_message = None
+
+    def focus_message(self, index: int, scroll_visible: bool = True) -> None:
+        self.children[index].focus(scroll_visible=scroll_visible)
+        self.focused_message = self.children[index]
+
+    async def mount_message(self, message) -> None:
+        await self.mount(Message(self.chat, message))
+        await self.children[-1].process()
+        await self.children[-1].finish()
+
+    async def action_continue(self) -> None:
+        if (self.messages[-1]["role"] == "user" or self.messages[-1] == "tool" or
+            self.messages[-1]["role"] == "assistant" and "tool_calls" in
+            self.messages[-1]):
+            work = Work(self.chat)
+            self.chat.work = self.run_worker(work.work_stream())
+
+    async def action_undo(self) -> None:
+        await self.chat.undo.undo()
+
+    async def action_redo(self) -> None:
+        await self.chat.undo.redo()
+
+    def check_action(self, action: str,
+                     parameters: tuple[object, ...]) -> bool | None:
+        match action:
+            case "continue":
+                if self.chat.is_working() or self.chat.edit or not self.messages:
+                    return False
+                if self.messages[-1]["role"] == "assistant":
+                    if not "tool_calls" in self.messages[-1]:
+                        return False
+            case "undo":
+                if self.chat.is_working() or self.chat.edit:
+                    return False
+                if self.chat.undo.undo_index == -1:
+                    return False
+            case "redo":
+                if self.chat.is_working() or self.chat.edit:
+                    return False
+                if self.chat.undo.undo_index == len(self.chat.undo.undo)-1:
+                    return False
+                if len(self.chat.undo.undo) == 0:
+                    return False
+        return True
