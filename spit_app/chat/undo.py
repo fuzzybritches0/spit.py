f76b461a6c9ceb093a31ea273d7b4a741523f754
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-17 23:32:35 +0000

    chat: undo: refactor
    Turn module into a class, move all variables of concern here.

--- spit_app/chat/undo.py
+++ spit_app/chat/undo.py
@@ -1,59 +1,59 @@
 # SPDX-License-Identifier: GPL-2.0
-import spit_app.chat.message as message
-import spit_app.chat.render as render
+class Undo:
+    def __init__(self, chat) -> None:
+        self.chat = chat
+        self.chat_view = chat.chat_view
+        self.app = chat.app
+        self.messages = chat.messages
+        self.undo = []
+        self.undo_index = -1
 
-async def undo(self) -> None:
-    if self.undo_index >= 0:
-        operation, umessage, index = self.undo[self.undo_index]
-        if operation == "remove":
-            self.messages.append(umessage.copy())
-            await render.message(self, umessage)
-            self.chat_view.focus_message(-1)
-        if operation == "append":
-            del self.messages[-1]
-            await message.remove_last_turn(self)
-            self.chat_view.focus_message(-1)
-        if operation == "change":
-            temp_umessage = self.messages[index].copy()
-            self.messages[index] = umessage.copy()
-            self.undo[self.undo_index] = [operation, temp_umessage.copy(), index]
-            self.edit = True
-            self.edit_container = self.query_one("#chat-view").children[index]
-            await render.message(self, umessage)
-            self.chat_view.focus_message(index)
-            self.edit = False
-        self.write_chat_history()
-        self.undo_index-=1
-        self.app.refresh_bindings()
+    async def undo(self) -> None:
+        if self.undo_index >= 0:
+            operation, message, index = self.undo[self.undo_index]
+            if operation == "remove":
+                self.messages.append(message.copy())
+                await self.chat.mount_message()
+                self.chat_view.focus_message(-1)
+            if operation == "append":
+                del self.messages[-1]
+                await self.chat.remove_last_message()
+                self.chat_view.focus_message(-1)
+            if operation == "change":
+                temp_message = self.messages[index].copy()
+                self.messages[index] = message.copy()
+                self.undo[self.undo_index] = [operation, temp_message.copy(), index]
+                await self.chat.update_message(index)
+                self.chat_view.focus_message(index)
+            self.chat.write_chat_history()
+            self.undo_index-=1
+            self.chat.refresh_bindings()
 
-async def redo(self) -> None:
-    if self.undo_index < len(self.undo)-1:
-        self.undo_index+=1
-        operation, umessage, index = self.undo[self.undo_index]
-        if operation == "remove":
-            del self.messages[-1]
-            await message.remove_last_turn(self)
-            self.chat_view.focus_message(-1)
-        if operation == "append":
-            self.messages.append(umessage.copy())
-            await render.message(self, umessage)
-            self.chat_view.focus_message(-1)
-        if operation == "change":
-            temp_umessage = self.messages[index].copy()
-            self.messages[index] = umessage.copy()
-            self.undo[self.undo_index] = [operation, temp_umessage.copy(), index]
-            self.edit = True
-            self.edit_container = self.query_one("#chat-view").children[index]
-            await render.message(self, umessage)
-            self.chat_view.focus_message(index)
-            self.edit = False
-        self.write_chat_history()
-        self.app.refresh_bindings()
+    async def redo(self) -> None:
+        if self.undo_index < len(self.undo)-1:
+            self.undo_index+=1
+            operation, message, index = self.undo[self.undo_index]
+            if operation == "remove":
+                del self.messages[-1]
+                await self.chat.remove_last_message()
+                self.chat_view.focus_message(-1)
+            if operation == "append":
+                self.messages.append(message.copy())
+                await self.chat.mount_message()
+                self.chat_view.focus_message(-1)
+            if operation == "change":
+                temp_message = self.messages[index].copy()
+                self.messages[index] = message.copy()
+                self.undo[self.undo_index] = [operation, temp_message.copy(), index]
+                await self.chat.update_message(index)
+                self.chat_view.focus_message(index)
+            self.chat.write_chat_history()
+            self.chat.refresh_bindings()
 
-def append_undo(self, operation: str, umessage: dict, index: int = -1) -> None:
-    while len(self.undo)-1 > self.undo_index:
-        del self.undo[-1]
-    while len(self.undo) > 100:
-        del self.undo[0]
-    self.undo.append([operation, umessage.copy(), index])
-    self.undo_index=len(self.undo)-1
+    def append_undo(self, operation: str, message: dict, index: int = -1) -> None:
+        while len(self.undo)-1 > self.undo_index:
+            del self.undo[-1]
+        while len(self.undo) > 100:
+            del self.undo[0]
+        self.undo.append([operation, message.copy(), index])
+        self.undo_index=len(self.undo)-1
