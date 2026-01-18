cb557b941a6275b0413c51a7a8289c439e3d7d9a
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-18 14:59:40 +0000

    chat: remove unused var and methods

--- spit_app/chat/chat.py
+++ spit_app/chat/chat.py
@@ -30,7 +30,6 @@ class Chat(Vertical):
         self.chat_prompt = content["prompt"]
         self.messages = content["messages"]
         self.work = None
-        self.edit = False
         self.chat_view = ChatView(self)
         self.text_area = ChatTextArea(self)
         self.undo = Undo(self)
@@ -89,10 +88,6 @@ class Chat(Vertical):
         self.undo.append_undo("append", umessage)
         self.write_chat_history()
 
-    async def remove_last_message(self) -> None:
-        del self.messages[-1]
-        await self.chat_view.children[-1].remove()
-
     def action_change_focus(self) -> None:
         self.text_area.focus()
 
@@ -108,10 +103,6 @@ class Chat(Vertical):
             case "abort":
                 return self.is_working()
         return True
-
-    async def update_message(self, index: int) -> None:
-        await self.chat_view.children[index].reset()
-        await self.chat_view.children[index].finish()
 
     async def on_mount(self) -> None:
         if not self.messages:
