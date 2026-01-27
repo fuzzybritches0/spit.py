9783952b96ee1c52ed01436c6e0ae6269f990527
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-27 21:31:50 +0000

    app: add central file I/O methods
    We only ever write and read .json files. Add central methods read_json()
    and write_json() to do that. Use the new dict path{} in settings to
    choose the right path for reading and writing json objects.

--- spit_app/app.py
+++ spit_app/app.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
+import json
 from textual.app import App, ComposeResult
 from textual.containers import Vertical, Horizontal
 from textual.widgets import Header, Footer
@@ -22,6 +23,7 @@ class SpitApp(ActionsMixIn, HandlersMixIn, App):
         super().__init__()
         #self.applog("LOG START:")
         self.settings = Settings(self)
+        self.path = self.settings.path
         self.settings.load()
         self.tool_call = ToolCall(self)
         self.watch(self.app, "theme", self.on_theme_changed, init=False)
@@ -42,3 +44,29 @@ class SpitApp(ActionsMixIn, HandlersMixIn, App):
             await self.query_one("#main").query_one(f"#{id}").remove()
         except:
             pass
+
+    def write_json(self, path, content) -> bool:
+        path = path.split("/")
+        if len(path) > 1:
+            file = self.path[path[0]] / path[1]
+        else:
+            file = self.path[path[0]]
+        try:
+            file.write_text(json.dumps(content))
+        except Exception as exc:
+            error = f"{type(exc).__name__}: {exc}"
+            return False
+        return True
+
+    def read_json(self, path) -> None|dict:
+        path = path.split("/")
+        if len(path) > 1:
+            file = self.path[path[0]] / path[1]
+        else:
+            file = self.path[path[0]]
+        try:
+            ret = json.loads(file.read_text())
+        except Exception as exc:
+            error = f"{type(exc).__name__}: {exc}"
+            return None
+        return ret
