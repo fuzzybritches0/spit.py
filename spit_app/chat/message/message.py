65232390b61d520d918fefae06985271de9450a5
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-29 05:23:54 +0000

    message: don't scroll_visible

--- spit_app/chat/message/message.py
+++ spit_app/chat/message/message.py
@@ -119,7 +119,7 @@ class Message(VerticalScroll):
         del self.messages[-1]
         if self.chat.write_chat_history():
             if len(self.chat.chat_view.children) > 2:
-                self.chat.chat_view.children[-2].focus()
+                self.chat.chat_view.children[-2].focus(scroll_visible=False)
             elif len(self.chat.chat_view.children) == 1:
                 self.chat.text_area.focus()
             await self.remove()
