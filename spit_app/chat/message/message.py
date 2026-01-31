4afd55aba202d0138e8e08585c8e4096a5164fc0
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-01-31 19:59:53 +0000

    chat: message: refactor
    Remove all code that handles parsing content for rendering. Put sections
    into message for reasoning, content, and tool_calls.

--- spit_app/chat/message/message.py
+++ spit_app/chat/message/message.py
@@ -1,12 +1,12 @@
 import json
-from textual.containers import VerticalScroll
 from textual.widgets import Markdown
-from .containers.part import Part
-from .containers.code import Code
-from .pattern_processing import PatternProcessing
+from textual.containers import VerticalScroll
+from .process import Process
 
 class Message(VerticalScroll):
     BINDINGS = [
+        ("s", "show_cot", "Show CoT"),
+        ("s", "hide_cot", "Hide CoT"),
         ("e", "edit_content", "Edit content"),
         ("c", "edit_cot", "Edit CoT"),
         ("t", "edit_tool", "Edit tool call"),
@@ -18,115 +18,76 @@ class Message(VerticalScroll):
         self.message = message
         self.messages = chat.messages
         self.chat = chat
+        self.role = self.message["role"]
+        self.classes = "message-container-" + self.role
         self.id = "message-id-" + str(len(self.chat.chat_view.children))
-        role = message["role"]
-        if role == "tool":
-            role = "user"
-        self.classes = "message-container-" + role
-        if (self.app.focused is self.app.query_one("#side-panel") or
-            self.app.focused is self.chat.text_area):
-            if not self.chat.is_working():
-                self.focus(scroll_visible=False)
-            else:
-                self.chat.chat_view.focused_message = self
-        else:
-            if self.chat.display:
-                self.focus(scroll_visible=False)
-        self.chat.chat_view.focused_message = self
-        self.reset_state()
+        self.done_reasoning = False
 
-    def reset_state(self) -> None:
-        self.pp = PatternProcessing(self)
-        self.pos = 0
-        self.target = None
-        self.lenreason = 0
-        self.process_busy = False
-        self.finish_pending = False
-        self.finished_content = False
+    def tool_call_arguments(self, arguments: str) -> str:
+        ret = ""
+        arguments = json.loads(arguments)
+        for argument in arguments.keys():
+            ret +=f"\n    - {argument}:"
+            if arguments[argument]:
+                if "\n" in arguments[argument]:
+                    ret += f"\n```\n{arguments[argument]}\n```"
+                else:
+                    ret += f" `{arguments[argument]}`"
+        return ret
+
+    def format_tool_calls(self) -> str:
+        tool_calls = "## TOOL CALLS\n"
+        for tool_call in self.message["tool_calls"]:
+            tool_calls += f"\n- function: `{tool_call['function']['name']}`\n  - arguments:\n"
+            tool_calls += self.tool_call_arguments(tool_call["function"]["arguments"]) + "\n\n---"
+        return tool_calls[:-5]
+
+    async def update_status(self, status: str) -> None:
+        if self.done_reasoning:
+            return None
+        if not self.status.source == status:
+            await self.status.update(status)
+        if not status:
+            self.done_reasoning = True
 
     async def reset(self) -> None:
-        self.display = False
-        await self.remove_children()
-        self.reset_state()
-        await self.process()
-        await self.finish()
-        self.display = True
-
-    async def format_tool_calls(self) -> str:
-        await self.tool_target.remove()
-        for tool_call in self.message["tool_calls"]:
-            content = "\n\n---\n\n"
-            content += f"- function call: `{tool_call['function']['name']}`"
-            content += f" id: `{tool_call['id']}`"
-            await self.mount(Markdown(content))
-            args = tool_call["function"]["arguments"]
-            arguments = json.loads(args)
-            for argument in arguments.keys():
-                await self.mount(Markdown(f"\n- {argument}:"))
-                await self.mount(Code(self.chat))
-                await self.children[-1].update(f"```\n{arguments[argument]}\n```")
-                await self.children[-1].update_code()
-        return content
-
-    async def process_tool_calls(self) -> None:
-        content = json.dumps(self.message["tool_calls"])
-        if len(content)-6 > 0:
-            lensource = len(self.tool_target.source)
-            await self.tool_target.append(content[lensource:-6])
-
-    async def process_content(self) -> None:
-        content = self.message["content"]
-        self.pp.part = ""
-        if len(content)-self.pos-self.pp.bsize > 0:
-            for pos in range(self.pos, len(content) - self.pp.bsize):
-                await self.pp.process_patterns(content[pos:])
-            await self.target.append(self.pp.part)
-            self.pos=pos+1
-
-    async def finish_content(self) -> None:
-        if self.finished_content:
-            return None
-        content = self.message["content"]
-        self.pp.part = ""
-        for pos in range(self.pos, len(content)):
-            await self.pp.process_patterns(content[pos:])
-        await self.target.append(self.pp.part)
-        self.finished_content = True
-
-    async def finish_tool_calls(self) -> None:
-        if "tool_calls" in self.message and self.message["tool_calls"]:
-            await self.format_tool_calls()
+       await self.reasoning.reset()
+       await self.content.reset()
+       await self.tool_calls.reset()
+       await self.process()
+       await self.finish()
 
     async def finish(self) -> None:
-        if self.process_busy:
-            self.finish_pending = True
-        else:
-            self.finish_pending = False
-            await self.finish_content()
-            await self.finish_tool_calls()
-            self.target = None
+        if "reasoning" in self.message:
+            await self.reasoning.finish(self.message["reasoning"])
+        if "content" in self.message:
+            await self.content.finish(self.message["content"])
+        if "tool_calls" in self.message:
+            await self.tool_calls.process(self.format_tool_calls())
+            await self.tool_calls.finish(self.format_tool_calls())
 
     async def process(self) -> None:
-        if self.process_busy:
-            return None
-        self.process_busy = True
-        if not self.target:
-            await self.mount(Part(), before="#tool-calls")
-        if "reasoning" in self.message and self.message["reasoning"]:
-            lenreason = len(self.message["reasoning"])
-            if lenreason > self.lenreason and not self.reasoning_target.source == "Thinking...":
-                await self.reasoning_target.update("Thinking...")
-                self.lenreason = lenreason
-        if "content" in self.message and self.message["content"]:
-            await self.reasoning_target.update("")
-            await self.process_content()
-        if "tool_calls" in self.message and self.message["tool_calls"]:
-            await self.reasoning_target.update("")
-            await self.finish_content()
-            await self.process_tool_calls()
-        self.process_busy = False
-        if self.finish_pending:
-            await self.finish()
+        if "reasoning" in self.message:
+            await self.update_status("Thinking...")
+            await self.reasoning.process(self.message["reasoning"])
+        if "content" in self.message:
+            if self.message["content"]:
+                await self.update_status("")
+            await self.content.process(self.message["content"])
+        if "tool_calls" in self.message:
+            if self.message["tool_calls"]:
+                await self.update_status("")
+            await self.tool_calls.process(json.dumps(self.message["tool_calls"]))
+
+    def action_show_cot(self) -> None:
+        self.reasoning.display = True
+        self.reasoning.disabled = False
+        self.app.refresh_bindings()
+
+    def action_hide_cot(self) -> None:
+        self.reasoning.display = False
+        self.reasoning.disabled = True
+        self.app.refresh_bindings()
 
     def action_edit_content(self) -> None:
         self.edit_message("content")
@@ -154,6 +115,11 @@ class Message(VerticalScroll):
         self.chat.write_chat_history()
         await self.remove()
 
+    def has_reasoning(self) -> bool:
+        if self.message["role"] == "assistant" and self.message["reasoning"]:
+            return True
+        return False
+
     def check_action(self, action: str,
                      parameters: tuple[object, ...]) -> bool | None:
         if not self is self.app.focused:
@@ -161,11 +127,21 @@ class Message(VerticalScroll):
         if self.chat.is_working() or self.chat.text_area.is_edit:
             return False
         match action:
+            case "show_cot":
+                if not self.has_reasoning():
+                    return False
+                if self.reasoning.display:
+                    return False
+            case "hide_cot":
+                if not self.has_reasoning():
+                    return False
+                if not self.reasoning.display:
+                    return False
             case "edit_content":
                 if not self.message["content"]:
                     return False
             case "edit_cot":
-                if not "reasoning" in self.message:
+                if not self.has_reasoning():
                     return False
             case "edit_tool":
                 if not "tool_calls" in self.message:
@@ -177,12 +153,19 @@ class Message(VerticalScroll):
                     return False
         return True
 
-    def on_focus(self) -> None:
-        self.chat.chat_view.focused_message = self
+    async def prepare(self) -> None:
+        self.status = Markdown()
+        await self.mount(self.status)
+        await self.status.update("Processing...")
+        self.reasoning = Process(False)
+        await self.mount(self.reasoning)
+        self.content = Process()
+        await self.mount(self.content)
+        self.tool_calls = Process()
+        await self.mount(self.tool_calls)
 
     async def on_mount(self) -> None:
-        self.reasoning_target = Markdown()
-        await self.mount(self.reasoning_target)
-        await self.reasoning_target.update("Processing...")
-        self.tool_target = Markdown(id="tool-calls")
-        await self.mount(self.tool_target)
+        await self.prepare()
+        if self.chat.has_focus_within:
+            self.focus(scroll_visible=False)
+        self.chat.chat_view.focused_message = self
