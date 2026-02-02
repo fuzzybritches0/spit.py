b167b1d5d2dcaeec319d732bc2ddc5e1d2a09be4
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-02 04:15:51 +0000

    manage: actions: add custom after_action method
    Sometimes we need to do more things for an action, add a custom function
    do be able to add these extra things.

--- spit_app/manage/actions.py
+++ spit_app/manage/actions.py
@@ -1,23 +1,22 @@
 # SPDX-License-Identifier: GPL-2.0
 
 class ActionsMixIn:
+    async def after_action(self, action: str) -> None:
+        await self.remove_children()
+        await self.select_main_screen()
+
     def action_duplicate(self) -> None:
         self.duplicate()
 
     async def action_delete(self) -> None:
         self.delete()
-        await self.remove_children()
-        await self.select_main_screen()
+        await self.after_action("delete")
 
     async def action_save(self) -> None:
         if self.validate_values_edit():
             self.store_values()
             self.save()
-            await self.app.maybe_remove("manage-chats")
-            await self.app.maybe_remove("new-chat")
-            await self.remove_children()
-            await self.select_main_screen()
+            await self.after_action("save")
 
     async def action_cancel(self) -> None:
-        await self.remove_children()
-        await self.select_main_screen()
+        await self.after_action("cancel")
