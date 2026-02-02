ce90a9805f5c0db2fd2ba117718ca5ff888aada4
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-02 04:31:20 +0000

    modal_screens: add info screen

--- spit_app/modal_screens.py
+++ spit_app/modal_screens.py
@@ -3,34 +3,40 @@ from textual.widgets import Static, Button, Footer
 from textual.screen import ModalScreen
 from textual.containers import Vertical, Center
 
-class LoadingScreen(ModalScreen):
-    def __init__(self) -> None:
-        super().__init__()
-        self.classes = "overlay"
-
-    def compose(self) -> ComposeResult:
-        with Vertical(id="loading-modal"):
-            yield Static("Loading...")
-        yield Footer()
-
-class ErrorScreen(ModalScreen):
+class Common(ModalScreen):
     BINDINGS = [("ctrl+q", "exit_app", "Quit")]
-
-    def __init__(self, exception: Exception) -> None:
-        super().__init__()
-        self.classes = "overlay"
-        self.exception = exception
-
-    def compose(self) -> ComposeResult:
-        with Vertical(id="error-modal"):
-            exception = f"ERROR:\n\n{type(self.exception).__name__}: {self.exception}"
-            yield Static(exception)
-            with Center():
-                yield Button("OK")
-        yield Footer()
 
     async def on_button_pressed(self, event: Button.Pressed) -> None:
         self.dismiss()
 
     def action_exit_app(self) -> None:
         self.app.action_exit_app()
+
+    def compose(self) -> ComposeResult:
+        with Vertical(id=f"{self.mtype}-modal"):
+            yield Static(self.text)
+            if not self.mtype == "loading":
+                with Center():
+                    yield Button("OK")
+        yield Footer()
+
+class LoadingScreen(Common):
+    def __init__(self) -> None:
+        super().__init__()
+        self.classes = "modal"
+        self.mtype = "loading"
+        self.text = "Loading..."
+
+class ErrorScreen(Common):
+    def __init__(self, exception: Exception) -> None:
+        super().__init__()
+        self.classes = "modal"
+        self.mtype = "error"
+        self.text = f"ERROR:\n\n{type(exception).__name__}: {exception}"
+
+class InfoScreen(Common):
+    def __init__(self, info: str) -> None:
+        super().__init__()
+        self.classes = "modal"
+        self.mtype = "info"
+        self.text = f"INFO:\n\n{self.info}"
