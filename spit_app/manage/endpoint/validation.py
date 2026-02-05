a13204d57aac42dbc7420de2d80c116b058ef1cd
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-05 19:46:11 +0000

    manage: endpoint: add validation info

--- spit_app/manage/endpoint/validation.py
+++ spit_app/manage/endpoint/validation.py
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
 
 class ValidationMixIn:
+    failed_valid_setting_name = ["must be unique!"]
     def valid_setting_name(self, value: str) -> bool:
         if not value:
             return False
         for manage in self.managed.keys():
-            if (not manage == self.uuid and
-                    self.managed[manage]["name"]["value"].strip() == value.strip()):
+            if not manage == self.uuid and self.managed[manage]["name"]["value"].strip() == value.strip():
                 return False
         return True
 
@@ -19,7 +19,9 @@ class ValidationMixIn:
         else:
             return True
 
-    def validate_add_setting(self) -> bool:
+    def validate_add_setting(self) -> tuple[bool, str]:
+        valid = True
+        failed = []
         setting = self.query_one("#new-setting")
         desc = self.query_one("#new-description")
         sel = None
@@ -29,10 +31,13 @@ class ValidationMixIn:
         setting.validate(setting.value)
         desc.validate(desc.value)
         if not setting.is_valid:
-            return False
+            valid = False
+            failed += ["- `Setting`: valid: (a-z0-9_-), start with (a-z), end with (a-z0-9), be unique!"]
         if not desc.is_valid:
-            return False
+            valid = False
+            failed += ["- `Description`: must not be empty!"]
         if sel:
             if not sel.is_valid:
-                return False
-        return True
+                valid = False
+                failed +=["- `Select values`: valid: (a-z0-9_-), start with (a-z), end with (a-z0-9), be unique!"]
+        return (valid, failed)
