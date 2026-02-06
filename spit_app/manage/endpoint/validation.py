f61ab54b55f0a2c62c969e7f9eba70500ec1661f
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-06 16:41:14 +0000

    manage: endpoint: validation: update methods
    Update validation methods for endpoint validation as well.

--- spit_app/manage/endpoint/validation.py
+++ spit_app/manage/endpoint/validation.py
@@ -1,25 +1,29 @@
 # SPDX-License-Identifier: GPL-2.0
 
 class ValidationMixIn:
-    failed_valid_setting_name = ["must be unique!"]
-    def valid_setting_name(self, value: str) -> bool:
-        if not value:
-            return False
-        for manage in self.managed.keys():
-            if not manage == self.uuid and self.managed[manage]["name"]["value"].strip() == value.strip():
-                return False
-        return True
+    def valid_setting_name(self, value: str) -> tuple:
+        valid = True
+        if value:
+            for manage in self.managed.keys():
+                if not manage == self.uuid and self.managed[manage]["name"]["value"].strip() == value.strip():
+                    valid = False
+                    break
+        if not valid:
+            return (False, "Must be unique!")
+        return (True, None)
 
-    def is_unique_custom(self, value: str) -> bool:
+    def is_unique_custom(self, value: str) -> tuple:
+        valid = True
         if value:
             for name in self.manage.keys():
                 if name.strip() == value.strip():
-                    return False
-            return True
-        else:
-            return True
+                    valid = False
+                    break
+        if not valid:
+            return (False, "Must be unique!")
+        return (True, None)
 
-    def validate_add_setting(self) -> tuple[bool, str]:
+    def validate_add_setting(self) -> tuple:
         valid = True
         failed = []
         setting = self.query_one("#new-setting")
