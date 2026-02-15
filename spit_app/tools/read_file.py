c74ceecadef61e3951a032c9c822086dff35eacb
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-15 13:26:20 +0000

    tools: read_file: refactor code

--- spit_app/tools/read_file.py
+++ spit_app/tools/read_file.py
@@ -1,5 +1,4 @@
 # SPDX-License-Identifier: GPL-2.0
-from pathlib import Path
 
 NAME = __file__.split("/")[-1][:-3]
 
@@ -29,10 +28,11 @@ SETTINGS = {
     
 
 def call(app, arguments: dict, chat_id) -> str:
-    location = str(app.settings.path["sandbox"]) + "/" + arguments["path"]
-    try:
-        with open(location, "r") as f:
-            content = f.read()
-        return arguments["path"] + ":\n\n```\n" + content + "\n```"
-    except Exception as exception:
-        return f"ERROR:\n\n{type(exception).__name__}: {exception}"
+    location = arguments["path"].replace("..", "")
+    if not location == arguments["path"]:
+        return "ERROR: location not allowed!"
+    location = location.lstrip("/")
+    location = app.settings.path["sandbox"] / location
+    with open(location, "r") as f:
+        content = f.read()
+    return arguments["path"] + ":\n\n```\n" + content + "\n```"
