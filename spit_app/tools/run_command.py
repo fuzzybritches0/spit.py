41b01ed7c3bef356dbd08894daa2f56a8af03d24
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-15 17:07:51 +0000

    tools: run_command: use class Sandbox

--- spit_app/tools/run_command.py
+++ spit_app/tools/run_command.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
 import os
-import shlex
 import shutil
 import asyncio
+from time import time
+from spit_app.tools.sandbox.sandbox import Sandbox
 from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
@@ -25,53 +26,43 @@ DESC = {
     }
 }
 
-PROMPT = "Use this function to run Shell commands and receive their output. You can only write files in the current directory."
+PROMPT = "Use this function to run Shell commands and receive their output."
 
 SETTINGS = {
     "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" }
 }
 
-def bwrap_args(app) -> list:
-    nobind = ["dev", "proc", "boot", "home", "tmp"]
-    args = ["bwrap"]
-    for d in os.listdir("/"):
-        if not d in nobind:
-            args += ["--bind", f"/{d}", f"/{d}"]
-    args += ["--chdir", "/home/user"]
-    args += ["--bind", app.settings.path['sandbox'], "/home/user"]
-    args += ["--share-net", "--unshare-all", "--proc", "/proc",
-             "--dev", "/dev", "--tmpfs", "/tmp", "--new-session"]
-    return args
-
 def preflight_tests(command) -> str|None:
-    FORBIDDEN_CHARS = set(";|&`$()<>")
-    if any(ch in command for ch in FORBIDDEN_CHARS):
-        return f"ERROR: shell metacharacters not permitted!"
-    cmd = command.split(" ", 1)
-    if not shutil.which(cmd[0]):
-        return f"ERROR: {cmd[0]} not in $PATH!"
-    if not shutil.which("bwrap"):
-        return "ERROR: bubblewrap not found! Please install with `apt install bubblewrap`!"
+    ret = ""
+    for cmd in ["bwrap", "bash"]:
+        if not shutil.which(cmd):
+            ret += f"ERROR: {cmd} not found! Give user instructions to install!\n"
+    if ret:
+        return ret
     return None
-
 
 async def call_async_generator(app, arguments: dict, chat_id):
     load_user_settings(app, NAME, SETTINGS)
     ret = preflight_tests(arguments["command"])
-    if ret:
-        yield ret
+    if not shutil.which("bash"):
+        yield "ERROR: bash not found! Give user instructions to install bash!"
     else:
-        cmd_args = bwrap_args(app) + shlex.split(arguments["command"])
-        yield "```\n"
-        proc = await asyncio.create_subprocess_exec(*cmd_args, stdout=asyncio.subprocess.PIPE,
-                                                stderr=asyncio.subprocess.STDOUT,
-                                                cwd=app.settings.path["sandbox"])
-        while True:
-            line_bytes = await proc.stdout.readline()
-            if not line_bytes:
-                break
-            yield line_bytes.decode("UTF-8", errors="replace")
-        if proc and proc.returncode is None:
-            proc.terminate()
-            await proc.wait()
-        yield "\n```"
+        if ret:
+            yield ret
+        else:
+            id = "." + str(time()) + chat_id
+            with open(app.settings.path["sandbox"] / id, "w") as f:
+                f.write(arguments["command"])
+            sandbox = Sandbox(app.settings.path["sandbox"], ["bash", f"./{id}"])
+            replace = f"./{id}: line 1: "
+            try:
+                async for line in sandbox.run_sandbox():
+                    if line.startswith(replace):
+                        linelen = len(replace)
+                        yield line[linelen:]
+                    else:
+                        yield line
+            except:
+                raise
+            finally:
+                os.remove(app.settings.path["sandbox"] / id)
