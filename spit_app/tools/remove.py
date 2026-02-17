c68c38338282b7f9c7e576db88b313190e538936
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-17 21:39:47 +0000

    tools: remove: use Run for sandboxing
    It is very difficult to contain this function to one directory. Simply
    use Run to sandbox the function.

--- spit_app/tools/remove.py
+++ spit_app/tools/remove.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
-import os
-import shutil
-from pathlib import Path
+from spit_app.tools.run.run import Run
+from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
 
@@ -24,27 +23,47 @@ DESC = {
 }
 
 PROMPT = "Use this function to remove a file or directory recursively."
+SANDBOX = True
 
 SETTINGS = {
-    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" }
+    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" },
+    "sandbox": { "value": SANDBOX, "stype": "boolean", "desc": "Run in sandbox (DANGER: Do not deactivate!)"}
 }
-    
-def call(app, arguments: dict, chat_id) -> str:
-    location = arguments["path"]
-    if ".." in location:
-        return "ERROR: location not allowed!"
-    location = app.settings.path["sandbox"] / location
-    if not os.path.exists(location):
-        return f"ERROR: {location} does not exist!"
-    if os.path.isfile(location):
-        try:
-            os.remove(location)
-            return f"{arguments['path']} removed."
-        except Exception as exception:
-            return f"ERROR:\n\n{type(exception).__name__}: {exception}"
-    if os.path.isdir(location):
-        try:
-            shutil.rmtree(location)
-            return f"{arguments['path']} removed."
-        except Exception as exception:
-            return f"ERROR:\n\n{type(exception).__name__}: {exception}"
+
+SCRIPT = """
+import os
+import sys
+import shutil
+
+if not os.path.exists(path):
+    print(f"ERROR: {path} does not exist!")
+    sys.exit(1)
+if os.path.isfile(path):
+    try:
+        os.remove(path)
+        print(f"{path} removed.")
+    except Exception as exception:
+        print(f"ERROR: {type(exception).__name__}: {exception}")
+        sys.exit(1)
+elif os.path.isdir(path):
+    try:
+        shutil.rmtree(path)
+        print(f"{path} removed.")
+    except Exception as exception:
+        print(f"ERROR: {type(exception).__name__}: {exception}")
+        sys.exit(1)
+"""
+
+EXEC = {
+    "script": SCRIPT,
+    "interpreter": "python3"
+}
+
+async def call_async_generator(app, arguments: dict, chat_id) -> str:
+    load_user_settings(app, NAME, SETTINGS)
+    args = f"path = \"{arguments['path']}\"\n"
+    script = args + EXEC["script"]
+    run = Run(app.settings.path["sandbox"], chat_id, EXEC["interpreter"], script,
+              SETTINGS["sandbox"]["value"], 0)
+    async for line in run.run():
+        yield line
