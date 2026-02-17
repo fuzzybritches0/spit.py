65c350cdbb11892a952fa40e971f56b5b46e7e87
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-17 14:26:07 +0000

    tools: run_command: adapt to Run class
    Adapt run_command to use the Run class, clean-up code, and adapt
    SETTINGS.

--- spit_app/tools/run_command.py
+++ spit_app/tools/run_command.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import asyncio
 from time import time
-from spit_app.tools.sandbox.sandbox import Sandbox
+from spit_app.tools.run.run import Run
 from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
@@ -26,43 +26,36 @@ DESC = {
     }
 }
 
+SANDBOX = True
+MAX_SECONDS = 0
 PROMPT = "Use this function to run Shell commands and receive their output."
+PROMPT_INST = "Timeout is set to [timeout]. When timeout is set to 0, there is no timeout limit."
 
 SETTINGS = {
-    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" }
+    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" },
+    "sandbox": { "value": SANDBOX, "stype": "boolean", "desc": "Run in sandbox"},
+    "timeout": { "value": MAX_SECONDS, "stype": "uinteger", "empty": False, "desc": "Timeout (0 = no timeout)"},
 }
-
-def preflight_tests(command) -> str|None:
-    ret = ""
-    for cmd in ["bwrap", "bash"]:
-        if not shutil.which(cmd):
-            ret += f"ERROR: {cmd} not found! Give user instructions to install!\n"
-    if ret:
-        return ret
-    return None
 
 async def call_async_generator(app, arguments: dict, chat_id):
     load_user_settings(app, NAME, SETTINGS)
-    ret = preflight_tests(arguments["command"])
     if not shutil.which("bash"):
-        yield "ERROR: bash not found! Give user instructions to install bash!"
-    else:
-        if ret:
-            yield ret
-        else:
-            id = "." + str(time()) + chat_id
-            with open(app.settings.path["sandbox"] / id, "w") as f:
-                f.write(arguments["command"])
-            sandbox = Sandbox(app.settings.path["sandbox"], ["bash", f"./{id}"])
-            replace = f"./{id}: line 1: "
-            try:
-                async for line in sandbox.run_sandbox():
-                    if line.startswith(replace):
-                        linelen = len(replace)
-                        yield line[linelen:]
-                    else:
-                        yield line
-            except:
-                raise
-            finally:
-                os.remove(app.settings.path["sandbox"] / id)
+        yield f"ERROR: `bash` not found! Give user instructions to install!\n"
+        return
+    id = "." + str(time()) + chat_id
+    with open(app.settings.path["sandbox"] / id, "w") as f:
+        f.write(arguments["command"])
+    run = Run(app.settings.path["sandbox"], ["bash", f"./{id}"],
+              SETTINGS["sandbox"]["value"], SETTINGS["timeout"]["value"])
+    replace = f"./{id}: line 1: "
+    try:
+        async for line in run.run():
+            if line.startswith(replace):
+                linelen = len(replace)
+                yield line[linelen:]
+            else:
+                yield line
+    except:
+        raise
+    finally:
+        os.remove(app.settings.path["sandbox"] / id)
