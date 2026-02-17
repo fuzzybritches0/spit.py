3ea164d5a52fe12326aa9ecc8aa157414301fde1
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-17 21:37:14 +0000

    tools: read_file: use Run for sandboxing
    It is very difficult to contain this function to one directory. Simply
    use Run to sandbox the function.

--- spit_app/tools/read_file.py
+++ spit_app/tools/read_file.py
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
+from spit_app.tools.run.run import Run
+from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
 
@@ -21,18 +23,33 @@ DESC = {
 }
 
 PROMPT = "Use this function to read the content of any type of text file."
+SANDBOX = True
 
 SETTINGS = {
-    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" }
+    "prompt": { "value": PROMPT, "stype": "text", "desc": "Prompt" },
+    "sandbox": { "value": SANDBOX, "stype": "boolean", "desc": "Run in sandbox (DANGER: Do not deactivate!)"}
 }
-    
 
-def call(app, arguments: dict, chat_id) -> str:
-    location = arguments["path"].replace("..", "")
-    if not location == arguments["path"]:
-        return "ERROR: location not allowed!"
-    location = location.lstrip("/")
-    location = app.settings.path["sandbox"] / location
-    with open(location, "r") as f:
-        content = f.read()
-    return arguments["path"] + ":\n\n```\n" + content + "\n```"
+SCRIPT = """
+import sys
+
+try:
+    with open(path, "r") as f:
+        print(f.read())
+except Exception as exception:
+    print(f"ERROR: {type(exception).__name__}: {exception}")
+    sys.exit(1)
+"""
+
+EXEC = {
+    "script": SCRIPT,
+    "interpreter": "python3"
+}
+
+async def call_async_generator(app, arguments: dict, chat_id) -> str:
+    load_user_settings(app, NAME, SETTINGS)
+    script = f"path = \"{arguments['path']}\"\n" + EXEC["script"]
+    run = Run(app.settings.path["sandbox"], chat_id, EXEC["interpreter"], script,
+              SETTINGS["sandbox"]["value"], 0)
+    async for line in run.run():
+        yield line
