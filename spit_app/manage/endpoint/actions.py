f0c76405477a30d305a8a63023612cda6e6e583e
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-02-28 18:09:24 +0000

    manage: endpoint: remove InfoScreen
    We no longer display validation errors in a modal Screen. Only proceed
    if validation succeeded, else do nothing.

--- spit_app/manage/endpoint/actions.py
+++ spit_app/manage/endpoint/actions.py
@@ -1,13 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
 from textual.widgets import Select
-from spit_app.modal_screens import InfoScreen
 
 class ActionsMixIn:
     async def action_add_setting(self) -> None:
-        valid, failed = self.validate_add_setting()
-        if not valid:
-            await self.app.push_screen(InfoScreen("\n".join(failed)))
-        else:
+        if self.validate_add_setting():
             setting = self.query_one("#new-setting").value
             stype = self.query_one("#custom-setting-select-add").value
             desc = self.query_one("#new-description").value
@@ -24,7 +20,7 @@ class ActionsMixIn:
     async def action_remove_setting(self) -> None:
         remove = self.query_one("#custom-setting-select-remove").value
         if not remove == Select.BLANK:
-            self.remove_custom_setting(remove)
+            await self.remove_custom_setting(remove)
             self.query_one("#custom-setting-select-remove").set_options(self.custom_options())
             remove = remove.replace(".", "-")
             await self.query_one(f"#{remove}").remove()
