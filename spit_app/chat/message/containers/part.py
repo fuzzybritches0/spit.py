a87d3d4f8beef29116b50f46f5acf888a97d1437
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-03-05 01:30:38 +0000

    message: containers: part: add Markdown stream
    To get a more responsive TUI we need to use the stream functionality of
    the Markdown class. Add the Markdown stream to the Part class.
    
    Also, add the logic to set the left margin of the Part widget. When
    markdown gets split between Code and LaTeX widgets, indentation gets
    lost since the Markdown removes extra spaces on the beginning of lines.
    Calculate the indentation that should be there and instead set the left
    margin of the widget to that indent. Create the Stream class intercept
    stream() and stop() and do the calculation after stop() has finished.

--- spit_app/chat/message/containers/part.py
+++ spit_app/chat/message/containers/part.py
@@ -1,4 +1,25 @@
 from textual.widgets import Markdown
+from markdown_it import MarkdownIt
+
+class Stream:
+    def __init__(self, widget) -> None:
+        self.widget = widget
+        self.stream = Markdown.get_stream(widget)
+        self.indent = None
+
+    async def write(self, part: str) -> None:
+        await self.stream.write(part)
+
+    async def stop(self) -> None:
+        await self.stream.stop()
+        indent = 30
+        for line in self.widget.source.split("\n"):
+            if line:
+                lline = line.lstrip(" ")
+                ind = len(line) - len(lline)
+                if indent > ind:
+                    indent = ind
+        self.widget.styles.margin = (0, 0, 0, indent)
 
 class Part(Markdown):
     def __init__(self) -> None:
@@ -6,4 +27,5 @@ class Part(Markdown):
         self.app.refresh_bindings()
 
     def on_mount(self) -> None:
+        self.stream = Stream(self)
         self.parent.target = self
