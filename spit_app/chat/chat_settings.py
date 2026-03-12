7177f364d66da4ca0bfbec59b17c1c693b08acc4
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-03-12 17:17:34 +0000

    chat: chat_settings: use @work(exclusive=True)
    1. Update model selects non-blocking in on_select_changed
    2. In on_mount, just mount the Selects with "None" option, then call
       update_selects().
    3. Use @work decorator with update_model_selects()
    4. Clean-up the mess and introduce update_selects(), get_options(), and
       set_selects().

--- spit_app/chat/chat_settings.py
+++ spit_app/chat/chat_settings.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-from textual import events
+from textual import events, work
 from textual.containers import Horizontal
 from textual.widgets import Label, Select
 from spit_app.endpoints.llamacpp import get_models_tuple
@@ -15,15 +15,18 @@ class ChatSettings(Horizontal):
         self.selects = [ 1, 3, 5]
 
     async def on_select_changed(self, event: Select.Changed) -> None:
-        if event.value == "none" or self.selects_updating:
+        if event.value == "none":
             return None
         if event.control.id == "select-endpoint":
             self.chat.chat_endpoint = event.value
-            self.query_one("#select-model").set_options(await self.model_options())
+            self.update_model_selects()
         if event.control.id == "select-model":
             self.chat.chat_model = event.value
         if event.control.id == "select-model-settings":
-            self.chat.chat_model_settings = event.value
+            if event.value == Select.BLANK:
+                self.chat.chat_model_settings = None
+            else:
+                self.chat.chat_model_settings = event.value
         self.chat.write_chat_history()
 
     def focus(self) -> None:
@@ -49,40 +52,45 @@ class ChatSettings(Horizontal):
         self.chat.focus()
 
     async def on_mount(self) -> None:
-        await self.update_selects()
-        self.disallowed_focus()
-
-    async def update_selects(self) -> None:
-        await self.remove_children()
-        options = self.endpoint_options()
+        options = (("None", "none"),)
         await self.mount(Label("Endpoint:"))
         await self.mount(Select(options, id="select-endpoint", allow_blank=False, compact=True))
-        self.children[-1].set_options(options)
-        self.children[-1].value = self.set_value(options, self.chat.chat_endpoint, False)
-        self.chat.chat_endpoint = self.children[1].value
-        options = await self.model_options()
         await self.mount(Label("Model:"))
         await self.mount(Select(options, id="select-model", allow_blank=False, compact=True))
-        self.children[-1].set_options(options)
-        self.children[-1].value = self.set_value(options, self.chat.chat_model, False)
-        self.chat.chat_model = self.children[3].value
-        options = self.model_settings_options()
         await self.mount(Label("Settings:"))
         await self.mount(Select(options, id="select-model-settings", allow_blank=True, compact=True))
-        self.children[-1].set_options(options)
-        self.children[-1].value = self.set_value(options, self.chat.chat_model_settings, True)
-        if self.children[-1].value is Select.BLANK:
-            self.chat.chat_model_settings = None
-        else:
-            self.chat.chat_model_settings = self.children[5].value
+        self.update_selects()
+        self.disallowed_focus()
+
+    @work(exclusive=True)
+    async def update_model_selects(self) -> None:
+        options = await self.model_options()
+        self.children[3].set_options(options)
+        self.children[3].value = self.set_value(options, self.chat.chat_model, False)
+        self.chat.chat_model = self.children[3].selection
+
+    def update_selects(self) -> None:
+        options = self.get_options()
+        self.children[1].set_options(options[0])
+        self.children[5].set_options(options[1])
+        self.set_selects(options)
+        self.update_model_selects()
+
+    def get_options(self) -> list:
+        options = []
+        options.append(self.endpoint_options())
+        options.append(self.model_settings_options())
+        return options
+
+    def set_selects(self, options: list) -> None:
+        self.children[1].value = self.set_value(options[0], self.chat.chat_endpoint, False)
+        self.chat.chat_endpoint = self.children[1].selection
+        self.children[5].value = self.set_value(options[1], self.chat.chat_model_settings, True)
+        self.chat.chat_model_settings = self.children[5].selection
         self.chat.write_chat_history()
 
     async def on_descendant_focus(self) -> None:
         self.allowed_focus()
-        options = await self.model_options()
-        self.children[3].set_options(options)
-        self.children[3].value = self.set_value(options, self.chat.chat_model, False)
-        self.chat.chat_model = self.children[3].value
 
     def on_descendant_blur(self) -> None:
         if not self.has_focus_within:
