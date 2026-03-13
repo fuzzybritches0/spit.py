97524aca256f8792a1e4ec7bfc6368648ac18556
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-03-13 02:34:42 +0000

    chat: messages: code clean-up
    Put all action_ related code into ActionsMixIn. Put related methods next
    to each other.

--- spit_app/chat/message/message.py
+++ spit_app/chat/message/message.py
@@ -1,18 +1,11 @@
-import json
 from textual.widgets import Markdown
 from textual.containers import VerticalScroll
 from .process import Process
 from .tool_calls import ToolCalls
+from .actions import ActionsMixIn, bindings
 
-class Message(VerticalScroll):
-    BINDINGS = [
-        ("s", "show_cot", "Show CoT"),
-        ("s", "hide_cot", "Hide CoT"),
-        ("e", "edit_content", "Edit content"),
-        ("c", "edit_cot", "Edit CoT"),
-        ("t", "edit_tool", "Edit tool call"),
-        ("x", "remove_last", "Remove turn")
-    ]
+class Message(VerticalScroll, ActionsMixIn):
+    BINDINGS = bindings
 
     def __init__(self, chat, message) -> None:
         super().__init__()
@@ -38,14 +31,6 @@ class Message(VerticalScroll):
         if not self.status.source == status:
             await self.status.update(status)
 
-    def get_current_process(self) -> None:
-        old_process = self.current_process
-        for process in self.processes:
-            if process in self.message and self.message[process]:
-                self.current_process = process
-        if not old_process == self.current_process:
-            self.finish_process = old_process
-
     def get_update(self, process: str) -> tuple:
         if process in self.message and self.message[process]:
             if process == "tool_calls":
@@ -56,14 +41,13 @@ class Message(VerticalScroll):
         else:
             return None, None
 
-    async def reset(self) -> None:
-        await self.pr["reasoning"].reset()
-        await self.pr["content"].reset()
-        await self.pr["tool_calls"].reset()
-        self.current_process = None
-        self.finish_process = None
-        self.tc = ToolCalls(self)
-        await self.finish()
+    def get_current_process(self) -> None:
+        old_process = self.current_process
+        for process in self.processes:
+            if process in self.message and self.message[process]:
+                self.current_process = process
+        if not old_process == self.current_process:
+            self.finish_process = old_process
 
     async def finish(self) -> None:
         await self.status.update("")
@@ -86,80 +70,14 @@ class Message(VerticalScroll):
         proc, update = self.get_update(self.current_process)
         await proc.process(update)
 
-    def action_show_cot(self) -> None:
-        self.pr["reasoning"].display = True
-        self.app.refresh_bindings()
-
-    def action_hide_cot(self) -> None:
-        self.pr["reasoning"].display = False
-        self.app.refresh_bindings()
-
-    def action_edit_content(self) -> None:
-        self.edit_message("content")
-
-    def action_edit_cot(self) -> None:
-        self.edit_message("reasoning")
-
-    def action_edit_tool(self) -> None:
-        self.edit_message("tool_calls")
-
-    def edit_message(self, ctype: str) -> None:
-        self.chat.text_area.temp = self.chat.text_area.text
-        if ctype == "tool_calls":
-            self.chat.text_area.text = json.dumps(self.message[ctype])
-        else:
-            self.chat.text_area.text = self.message[ctype]
-        self.chat.text_area.is_edit = True
-        self.chat.text_area.ctype = ctype
-        self.chat.text_area.edit_container = self
-        self.chat.text_area.focus()
-
-    async def action_remove_last(self) -> None:
-        self.chat.undo.append_undo("remove", self.message)
-        del self.messages[-1]
-        self.chat.write_chat_history()
-        if len(self.chat.chat_view.children) > 1:
-            self.chat.chat_view.children[-2].focus(scroll_visible=False)
-        await self.remove()
-
-    def has_reasoning(self) -> bool:
-        if self.message["role"] == "assistant" and self.message["reasoning"]:
-            return True
-        return False
-
-    def check_action(self, action: str,
-                     parameters: tuple[object, ...]) -> bool | None:
-        if not self is self.app.focused:
-            return False
-        if ((self.chat.is_working() or self.chat.text_area.is_edit) and
-            not (action == "show_cot" or action == "hide_cot")):
-            return False
-        match action:
-            case "show_cot":
-                if not self.has_reasoning():
-                    return False
-                if self.pr["reasoning"].display:
-                    return False
-            case "hide_cot":
-                if not self.has_reasoning():
-                    return False
-                if not self.pr["reasoning"].display:
-                    return False
-            case "edit_content":
-                if not self.message["content"]:
-                    return False
-            case "edit_cot":
-                if not self.has_reasoning():
-                    return False
-            case "edit_tool":
-                if not "tool_calls" in self.message:
-                    return False
-            case "remove_last":
-                if not self.parent.children:
-                    return False
-                if not self is self.parent.children[-1]:
-                    return False
-        return True
+    async def reset(self) -> None:
+        await self.pr["reasoning"].reset()
+        await self.pr["content"].reset()
+        await self.pr["tool_calls"].reset()
+        self.current_process = None
+        self.finish_process = None
+        self.tc = ToolCalls(self)
+        await self.finish()
 
     async def prepare(self) -> None:
         self.status = Markdown()
