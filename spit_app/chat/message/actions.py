902432d57616b639061e011677f98ae30695dda5
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-03-18 10:02:27 +0000

    chat: message: actions: remove edit action
    The edit action is now handled by Process. Remove the functionality from
    Message.

--- spit_app/chat/message/actions.py
+++ spit_app/chat/message/actions.py
@@ -3,9 +3,6 @@ import json
 bindings = [
     ("s", "show_cot", "Show CoT"),
     ("s", "hide_cot", "Hide CoT"),
-    ("e", "edit_content", "Edit content"),
-    ("c", "edit_cot", "Edit CoT"),
-    ("t", "edit_tool", "Edit tool call"),
     ("x", "remove_last", "Remove turn")
 ]
 
@@ -15,17 +12,10 @@ class ActionsMixIn:
         self.app.refresh_bindings()
 
     def action_hide_cot(self) -> None:
+        if self.children[1].children[0].has_focus:
+            self.focus(scroll_visible=False)
         self.pr["reasoning"].display = False
         self.app.refresh_bindings()
-
-    def action_edit_content(self) -> None:
-        self.edit_message("content")
-
-    def action_edit_cot(self) -> None:
-        self.edit_message("reasoning")
-
-    def action_edit_tool(self) -> None:
-        self.edit_message("tool_calls")
 
     async def action_remove_last(self) -> None:
         self.chat.undo.append_undo("remove", self.message)
@@ -35,26 +25,12 @@ class ActionsMixIn:
             self.chat.chat_view.children[-2].focus(scroll_visible=False)
         await self.remove()
 
-    def edit_message(self, ctype: str) -> None:
-        self.chat.text_area.temp = self.chat.text_area.text
-        if ctype == "tool_calls":
-            self.chat.text_area.text = json.dumps(self.message[ctype])
-        else:
-            self.chat.text_area.text = self.message[ctype]
-        self.chat.text_area.is_edit = True
-        self.chat.text_area.ctype = ctype
-        self.chat.text_area.edit_container = self
-        self.chat.text_area.focus()
-
     def has_reasoning(self) -> bool:
         if self.message["role"] == "assistant" and self.message["reasoning"]:
             return True
         return False
 
-    def check_action(self, action: str,
-                     parameters: tuple[object, ...]) -> bool | None:
-        if not self is self.app.focused:
-            return False
+    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
         if ((self.chat.is_working() or self.chat.text_area.is_edit) and
             not (action == "show_cot" or action == "hide_cot")):
             return False
@@ -69,18 +45,9 @@ class ActionsMixIn:
                     return False
                 if not self.pr["reasoning"].display:
                     return False
-            case "edit_content":
-                if not self.message["content"]:
-                    return False
-            case "edit_cot":
-                if not self.has_reasoning():
-                    return False
-            case "edit_tool":
-                if not "tool_calls" in self.message:
-                    return False
             case "remove_last":
-                if not self.parent.children:
+                if not self.chat.chat_view.children:
                     return False
-                if not self is self.parent.children[-1]:
+                if not self is self.chat.chat_view.children[-1]:
                     return False
         return True
