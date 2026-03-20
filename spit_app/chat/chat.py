11c7a81d59b77ce4b18cbb3ce76924d4f6209d22
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-03-20 11:53:52 +0000

    chat: multimodal: image support
    Allow the user to add images to messages if the model supports it.

--- spit_app/chat/chat.py
+++ spit_app/chat/chat.py
@@ -1,16 +1,26 @@
 # SPDX-License-Identifier: GPL-2.0
+import os
+import asyncio
+from textual import work
 from textual.app import ComposeResult
 from textual.containers import Vertical
 from .undo import Undo
 from .chat_text_area import ChatTextArea
 from .chat_settings import ChatSettings
 from .chat_view import ChatView
+from .multimodal import load_image_base64
+from spit_app.modal_screens import ChooseImageFile
+from spit_app.chat.message.message import Message
+
+def image_url(base64_image) -> dict:
+    return {"type": "image_url", "image_url": {"url": base64_image}}
 
 class Chat(Vertical):
     BINDINGS = [
         ("ctrl+escape", "abort", "Abort"),
         ("escape", "change_focus", "Focus"),
-        ("ctrl+s", "settings", "Settings")
+        ("ctrl+s", "settings", "Settings"),
+        ("ctrl+i", "add_image", "Add image")
     ]
 
     def __init__(self, id: str) -> None:
@@ -72,12 +82,33 @@ class Chat(Vertical):
     def action_settings(self) -> None:
         self.query_one("#select-endpoint").action_show_overlay()
 
+    @work
+    async def action_add_image(self) -> None:
+        file = await self.app.push_screen_wait(ChooseImageFile())
+        if file and os.path.exists(file.path) and os.path.isfile(file.path):
+            image = await asyncio.to_thread(load_image_base64, file.path)
+            if image:
+                if self.messages and self.messages[-1]["role"] == "user":
+                    self.undo.append_undo("change", self.messages[-1])
+                    self.messages[-1]["role"]["content"].append(image_url(image))
+                else:
+                    self.messages.append({"role": "user", "content": [image_url(image)]})
+                    self.undo.append_undo("append", self.messages[-1])
+                    await self.chat_view.mount(Message(self, self.messages[-1]))
+                self.write_chat_history()
+                await self.chat_view.children[-1].finish()
+                self.chat_view.focus()
+                self.chat_view.scroll_end(animate=False)
+
     def check_action(self, action: str,
                      parameters: tuple[object, ...]) -> bool | None:
         if action == "abort":
             return self.is_working()
         elif action == "settings":
             return not self.is_working()
+        elif action == "add_image":
+            if not "multimodal" in self.model_capabilities or self.is_working() or self.text_area.is_edit:
+                return False
         return True
 
     async def on_mount(self) -> None:
