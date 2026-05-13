e2982a8876b5f1141cc7499cf500f6a1c281064e
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-05-13 11:37:32 +0000

    chat: multimodal: allow URL images, too
    We want to be able not only to load images locally stored, but images
    from URLs.

--- spit_app/chat/multimodal.py
+++ spit_app/chat/multimodal.py
@@ -1,12 +1,18 @@
 import base64
 from PIL import Image, ImageOps
 from io import BytesIO
+import urllib.request
 
 def image_url(base64_image) -> dict:
     return {"type": "image_url", "image_url": {"url": base64_image}}
 
-def load_image_base64(file: str) -> str:
-    image = Image.open(file).convert("RGB")
+def load_image_base64(url: str) -> str:
+    if url.startswith("http://") or url.startswith("https://"):
+        response = urllib.request.urlopen(url)
+        image_data = response.read()
+        image = Image.open(BytesIO(image_data)).convert("RGB")
+    else:
+        image = Image.open(url).convert("RGB")
     image = ImageOps.exif_transpose(image)
     width, height = image.size
     max_dimension = 1600
