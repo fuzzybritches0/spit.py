9ebc288f7121fb39b36c8a3edd783574703030fa
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-05-21 23:40:03 +0000

    tools: remove: use backticks for variables

--- spit_app/tools/scripts/remove.py
+++ spit_app/tools/scripts/remove.py
@@ -3,19 +3,19 @@ import sys
 import shutil
 
 if not os.path.exists(path):
-    print(f"ERROR: {path} does not exist!")
+    print(f"ERROR: `{path}` does not exist!")
     sys.exit(1)
 if os.path.isfile(path):
     try:
         os.remove(path)
-        print(f"{path} removed.")
+        print(f"`{path}` removed.")
     except Exception as exception:
-        print(f"ERROR: {type(exception).__name__}: {exception}")
+        print(f"ERROR: `{type(exception).__name__}`: `{exception}`")
         sys.exit(1)
 elif os.path.isdir(path):
     try:
         shutil.rmtree(path)
-        print(f"{path} removed.")
+        print(f"`{path}` removed.")
     except Exception as exception:
-        print(f"ERROR: {type(exception).__name__}: {exception}")
+        print(f"ERROR: `{type(exception).__name__}`: `{exception}`")
         sys.exit(1)
