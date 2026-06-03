e4bb3e95f2c68b6db31bcb139f53d0bae5bc3a73
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-03 05:03:44 +0000

    manage: add InputWidget
    Replace ugly mount_setting() in .screens with class InputWidget.

---
+++ spit_app/manage/input_widget.py
@@ -0,0 +1,73 @@
+from textual.widgets import OptionList, Input, TextArea, Switch, Label, Select, SelectionList, Markdown
+from .validation import rid, fid
+
+class InputWidget:
+    def __init__(self, manage: dict, validators: callable):
+        self.manage = manage
+        self.validators = validators
+
+    def get_setting(self, setting: str) -> tuple:
+        value = ""
+        if "value" in self.manage[setting]:
+            value = self.manage[setting]["value"]
+        stype = self.manage[setting]["stype"]
+        desc = self.manage[setting]["desc"]
+        return str(value), stype, desc
+
+    async def get_tuple(self, setting: str) -> tuple:
+        if "ameth" in self.manage[setting]:
+            ameth = getattr(self, self.manage[setting]["ameth"])
+            if inspect.iscoroutinefunction(ameth):
+                return await ameth()
+            else:
+                return ameth()
+        if "options" in self.manage[setting]:
+            tup = ()
+            for el in self.manage[setting]["options"]:
+                tup += ((el, el),)
+            return tup
+        return ()
+
+    def get_widget(self, stype: str) -> str:
+        if stype in ["integer", "uinteger", "float", "ufloat", "url"]:
+            return "string"
+        else:
+            return stype
+
+    async def setting(self, setting: str) -> list:
+        id = rid(setting)
+        value, stype, desc = self.get_setting(setting)
+        tup = await self.get_tuple(setting)
+        ret = [Label(f"{desc}: ({stype})", id="label-" + id)]
+        return ret + getattr(self, self.get_widget(stype))(id, value, stype, desc, tup)
+
+    def select(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        if not value or not value in (i for n, i in tup):
+            value = Select.NULL
+        return [Select(tup, id=id, value=value, prompt="Default")]
+        
+    def select_no_default(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        if not value or not value in (i for n, i in tup):
+            value = tup[0][1]
+        return [Select(tup, id=id, value=value, allow_blank=False)]
+
+    def select_list(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        if not value:
+            value = []
+        ltup = ()
+        for n, i in tup:
+            if n in value:
+                ltup += ((n, i, True),)
+            else:
+                ltup += ((n, i, False),)
+        return [SelectionList(*ltup, id=id)]
+
+    def boolean(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        return [Switch(id=id, value=value)]
+
+    def text(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        return [TextArea(value, id=id, classes="text-area"), Markdown(id=f"val-{id}")]
+
+    def string(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+        validators = self.validators(fid(id), id, stype)
+        return [Input(validators=validators, id=id, value=value), Markdown(id=f"val-{id}")]
