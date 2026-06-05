13a5e2834cae0d11741e89bd7623b735a9c38947
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-05 04:32:17 +0000

    chat: message: tool_calls: fix arguments parsing
    There is more to it than one had wished. Values can be JSON and not
    start with " but with [ or {. We need to keep track of that, too. Some
    LLMs ouput all values beginning with ". Others don't. Well, lets see how
    far this implementation can carry us this time around. Testing so far
    yielded all positive results. Let's see what future LLMs will do with
    it.

--- spit_app/chat/message/tool_calls.py
+++ spit_app/chat/message/tool_calls.py
@@ -11,30 +11,50 @@ class ToolCalls:
         self.pos = 0
         self.skip = False
         self.mark = 0
-        self.key = True
+        self.key = False
+        self.value = False
+        self.json = []
 
     def tool_call_arguments(self, arguments: str) -> None:
         ret = ""
         pos = self.pos
         for pos in range(self.pos, len(arguments)):
             char = arguments[pos:pos+1]
-            if char == "{" and self.mark == 0:
-                self.skip = True
-            elif char == '"' and not self.last_char == "\\":
-                self.mark += 1
-                self.skip = True
-                if self.key:
-                    ret += "`"
-            elif char == "`" and self.key:
-                self.mark +=1
-            elif char == ":" and self.mark % 2 == 0:
-                self.skip = True
-                self.key = False
-                ret += "\n~~~~\n"
-            elif (char == "," or char == "}") and self.mark % 2 == 0 and not self.key:
-                self.skip = True
-                self.key = True
-                ret += "\n~~~~\n"
+            if (char == "{" or char == "[") and not self.value and not self.key:
+                if len(self.json) == 0:
+                    self.skip = True
+                    self.key = True
+                if len(self.json) == 1 and self.mark % 2 == 0:
+                    self.mark += 1
+                self.json += [char]
+            elif (char == "}" or char == "]") and not self.value and not self.key:
+                del self.json[-1]
+                if len(self.json) == 1 and self.mark % 2 == 1:
+                    ret += f"{char}\n~~~~\n"
+                    self.skip = True
+                    self.mark += 1
+                elif len(self.json) == 0:
+                    self.skip = True
+            if len(self.json) == 1:
+                if char == '"' and not self.last_char == "\\":
+                    self.mark += 1
+                    self.skip = True
+                    if self.key:
+                        ret += "`"
+                    elif self.value:
+                        self.value = False
+                    else:
+                        self.value = True
+                elif char == "`" and self.key:
+                    self.mark +=1
+                elif char == ":" and self.mark % 2 == 0 and self.key:
+                    self.skip = True
+                    self.key = False
+                    ret += "\n~~~~\n"
+                elif char == "," and self.mark % 2 == 0 and not self.key:
+                    self.skip = True
+                    self.key = True
+                    ret += "\n~~~~\n"
             self.last_char = char
             if self.skip:
                 self.skip = False
