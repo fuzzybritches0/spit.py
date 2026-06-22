58ee605837e28dfd6a3f010181f0e3f6926d54fb
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-22 19:03:40 +0000

    manage: input_widget: add custom SelectMethod
    Put all logic into a custom Select widget for updating options when we
    want to update them from a method. Clean up the rest of the module.

--- spit_app/manage/input_widget.py
+++ spit_app/manage/input_widget.py
@@ -2,8 +2,33 @@ import inspect
 from textual.widgets import OptionList, Input, TextArea, Switch, Label, Select, SelectionList, Markdown
 from .validation import rid, fid
 
+class SelectMethod(Select):
+    def __init__(self, *args, **kwargs):
+        if "method" in kwargs:
+            self.method = kwargs["method"]
+            del kwargs["method"]
+        super().__init__(*args, **kwargs)
+
+    async def method_get_options(self) -> tuple:
+            if inspect.iscoroutinefunction(self.method):
+                return await self.method()
+            else:
+                return self.method()
+
+    async def method_update_options(self) -> None:
+        if self.method:
+            options = await self.method_get_options()
+            if not self._allow_blank and not options:
+                options = (("None", "none"),)
+            self.set_options(options)
+            if not self.value or not self.value in (i for n, i in options):
+                if self._allow_blank:
+                    self._value = self.NULL
+                else:
+                    self._value = options[0][1]
+
 class InputWidget:
-    def __init__(self, methods, manage: dict, validators: callable):
+    def __init__(self, methods, manage: dict, validators: callable) -> None:
         self.methods = methods
         self.manage = manage
         self.validators = validators
@@ -19,19 +44,24 @@ class InputWidget:
         else:
             return str(value), stype, desc
 
-    async def get_tuple(self, setting: str) -> tuple:
+    def get_method(self, setting: str) -> callable:
         if "ameth" in self.manage[setting]:
-            ameth = getattr(self.methods, self.manage[setting]["ameth"])
-            if inspect.iscoroutinefunction(ameth):
-                return await ameth()
-            else:
-                return ameth()
+            return getattr(self.methods, self.manage[setting]["ameth"])
+        return None
+
+    async def get_method_options(self, setting: str, method: callable) -> tuple:
+        if inspect.iscoroutinefunction(method):
+            return await method()
+        else:
+            return method()
+
+    def get_options(self, setting: str) -> tuple|None:
         if "options" in self.manage[setting]:
             tup = ()
             for el in self.manage[setting]["options"]:
                 tup += ((el, el),)
             return tup
-        return ()
+        return None
 
     def get_widget(self, stype: str) -> str:
         if stype in ["select", "select_no_default", "select_list", "boolean", "text"]:
@@ -42,21 +72,24 @@ class InputWidget:
     async def setting(self, setting: str) -> list:
         id = rid(setting)
         value, stype, desc = self.get_setting(setting)
-        tup = await self.get_tuple(setting)
+        tup = self.get_options(setting)
+        method = self.get_method(setting)
+        if method:
+            tup = await self.get_method_options(setting, method)
         ret = [Label(f"{desc}: ({stype})", id="label-" + id)]
-        return ret + getattr(self, self.get_widget(stype))(id, value, stype, desc, tup)
+        return ret + getattr(self, self.get_widget(stype))(id, value, stype, method, tup)
 
-    def select(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def select(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         if not value or not value in (i for n, i in tup):
             value = Select.NULL
-        return [Select(tup, id=id, value=value, prompt="Default")]
+        return [SelectMethod(tup, id=id, value=value, prompt="Default", method=method)]
         
-    def select_no_default(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def select_no_default(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         if not value or not value in (i for n, i in tup):
             value = tup[0][1]
-        return [Select(tup, id=id, value=value, allow_blank=False)]
+        return [SelectMethod(tup, id=id, value=value, allow_blank=False, method=method)]
 
-    def select_list(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def select_list(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         if not value:
             value = []
         ltup = ()
@@ -67,12 +100,12 @@ class InputWidget:
                 ltup += ((n, i, False),)
         return [SelectionList(*ltup, id=id)]
 
-    def boolean(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def boolean(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         return [Switch(id=id, value=value)]
 
-    def text(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def text(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         return [TextArea(value, id=id, classes="text-area"), Markdown(id=f"val-{id}")]
 
-    def string(self, id: str, value: str, stype: str, desc: str, tup: tuple) -> list:
+    def string(self, id: str, value: str, stype: str, method: callable, tup: tuple) -> list:
         validators = self.validators(id, stype)
         return [Input(validators=validators, id=id, value=value), Markdown(id=f"val-{id}")]
