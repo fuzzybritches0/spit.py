3dbe06074ac504988ff49574597ecb2d08223289
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-25 20:12:48 +0000

    tools: run: persist bash env and /tmp
    Have the Bash environment persist between tool calls by storing exported
    variables for subsequent tool calls. More specifically, when using
    `run_command` store any exported variables and make them available for
    subsequent tool calls, matching the behaviour of an interactive Bash
    environment. Also, keep set variables available for subsequent
    `run_command` invocations, also matching the behaviour of an interactive
    Bash environment. Also, use the host `/tmp` directory to have any files
    stored by tool calls persist from tool call to tool call.

--- spit_app/tools/run/run.py
+++ spit_app/tools/run/run.py
@@ -6,6 +6,8 @@ import asyncio
 import signal
 from pathlib import Path
 
+SANDBOX_ENV = "/".join(__file__.split("/")[:-1]) + "/sandbox_env.sh"
+
 def get_script(tool, common: str = "") -> str:
     ret = ""
     script_dir = "/".join(tool.split("/")[0:-1])
@@ -21,7 +23,7 @@ def get_script(tool, common: str = "") -> str:
 class Run:
     def __init__(self, app, chat_id: str, cmd: str, script: str,
                  sandbox: bool = True, timeout: int = 0):
-        sandbox_home = app.query_one("#main").query_one(f"#{chat_id}").csettings["sandbox"]["value"]
+        sandbox_home = app.query_one("#main").query_one(f"#{chat_id}").cs("sandbox")
         self.sandbox_path = app.settings.path["sandbox"] / sandbox_home
         self.sandbox_path.mkdir(parents=True, exist_ok=True)
         self.cmd = [cmd]
@@ -29,9 +31,8 @@ class Run:
         self.sandbox = sandbox
         self.timeout = timeout
 
-    def bwrap_args(self) -> list:
-        user = getpass.getuser()
-        nobind = ["dev", "proc", "boot", "home", "tmp"]
+    def bwrap_args(self, user: str) -> list:
+        nobind = ["dev", "proc", "boot", "home"]
         args = ["bwrap"]
         for d in os.listdir("/"):
             if not d in nobind:
@@ -40,20 +41,23 @@ class Run:
         args += ["--setenv", "PIP_USER", "True"]
         args += ["--chdir", f"/home/{user}"]
         args += ["--bind", self.sandbox_path, f"/home/{user}"]
+        args += ["--bind", SANDBOX_ENV, f"/home/{user}/.sandbox_env.sh"]
         args += ["--unshare-all", "--share-net", "--proc", "/proc",
-                 "--dev", "/dev", "--tmpfs", "/tmp", "--new-session"]
+                 "--dev", "/dev", "--new-session"]
         return args
 
     async def run(self):
+        user = getpass.getuser()
         if self.sandbox and not shutil.which("bwrap"):
             yield "ERROR: `bwrap` not found! Give user instructions to install `bubblewrap`!"
             return
         if not shutil.which(self.cmd[0]):
             yield f"ERROR: `{self.cmd[0]}` not found!"
             return
-        cmd_args = self.cmd
         if self.sandbox:
-            cmd_args = self.bwrap_args() + self.cmd
+            cmd_args = self.bwrap_args(user) + [f"/home/{user}/.sandbox_env.sh"] + self.cmd
+        else:
+            cmd_args = [SANDBOX_ENV] + self.cmd
         proc = await asyncio.create_subprocess_exec(*cmd_args,
                         stdin=asyncio.subprocess.PIPE,
                         stdout=asyncio.subprocess.PIPE,
