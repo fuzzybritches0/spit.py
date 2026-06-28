8619d53967d304d021a17cfee8a80f1320ab676f
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-28 11:03:09 +0000

    process: text_area_edit: add content editor

---
+++ spit_app/chat/message/content/process/text_area_edit.py
@@ -0,0 +1,38 @@
+from textual.widgets import TextArea
+
+class TextAreaEdit(TextArea):
+    BINDINGS = [
+        ("ctrl+enter", "save", "Save"),
+        ("ctrl+escape", "cancel", "Cancel")
+    ]
+
+    def __init__(self, process, text: str):
+        super().__init__()
+        self.process = process
+        self.chat_view = process.chat_view
+        self.message = process.message
+        self.chat = process.chat
+        self.text = text
+        self.old_text = text
+        self.styles.height = "auto"
+
+    def on_mount(self) -> None:
+        self.focus()
+
+    async def action_save(self) -> None:
+        if not self.text == self.old_text:
+            index = self.chat_view.messages.index(self.message.message)
+            self.chat.undo.append_undo("change", self.message.message, index)
+            if type(self.message.message[self.process.scontent]) is str:
+                self.message.message[self.process.scontent] = self.text
+            else:
+                self.message.message[self.process.scontent][self.process.count]["text"] = self.text
+            self.chat.write_chat_history()
+        await self.action_cancel()
+
+    async def action_cancel(self) -> None:
+        async with self.process.batch():
+            await self.process.reset()
+            await self.process.finish(self.text)
+        self.message.is_edit -= 1
+        self.process.is_edit = False
