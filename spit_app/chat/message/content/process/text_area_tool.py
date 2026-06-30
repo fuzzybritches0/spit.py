397f5547e7a0769625ae826e09bfb96abed26b34
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-06-30 16:34:55 +0000

    process: text_area_tool: allow selecting tool
    Allow selecting tool, so the user can change the tool call completely.
    Fix issue with undo/redo - use deepcopy. Fix init() so we can change the
    tool call.

--- spit_app/chat/message/content/process/text_area_tool.py
+++ spit_app/chat/message/content/process/text_area_tool.py
@@ -1,5 +1,6 @@
 import json
-from textual.widgets import TextArea as _TextArea, Markdown
+from copy import deepcopy
+from textual.widgets import TextArea as _TextArea, Label, Select
 from textual.containers import Vertical
 from .tool_call import ToolCall
 
@@ -11,52 +12,69 @@ class TextArea(_TextArea):
         self._background = self.styles.background
 
     def on_text_area_changed(self) -> None:
-        if not self.text:
+        if not self.text and self.required:
             self.styles.background = "red"
         else:
             self.styles.background = self._background
 
 class TextAreaTool():
-    def __init__(self, process, tool: dict):
+    def __init__(self, process):
         self.process = process
+        self.chat = process.chat
         self.chat_view = process.chat_view
         self.message = process.message
         self.chat = process.chat
-        self.tool = tool
-        self.init()
+        self.tool_change = None
 
     def init(self) -> None:
-        self.ori_tool = self.tool
+        self.tool = deepcopy(self.message.message["tool_calls"][self.process.count])
+        if self.tool_change:
+            self.tool["function"]["name"] = self.tool_change
+        self.ori_tool = deepcopy(self.tool)
         self.old_tool = json.dumps(self.tool)
         self.arguments =  json.loads(self.tool["function"]["arguments"])
-        self.tool_name = self.tool["function"]["name"]
+        tool_name = self.tool["function"]["name"]
         self.unknown_tool = True
-        if self.tool_name in self.process.app.tool_call.tools:
+        if tool_name in self.process.app.tool_call.tools:
             self.unknown_tool = False
-            self.known_tool = self.process.app.tool_call.tools[self.tool_name]
+            self.known_tool = self.process.app.tool_call.tools[tool_name]
             self.properties = self.known_tool["desc"]["function"]["parameters"]["properties"]
             self.required = self.known_tool["desc"]["function"]["parameters"]["required"]
 
-    async def mount(self) -> None:
+    async def select_tool(self) -> None:
+        tools = ()
+        for tool in self.chat.cs("tools"):
+            tools += ((tool, tool),)
+        await self.process.mount(Select(tools, id="select-tool", value=self.tool["function"]["name"],
+                                allow_blank=False))
+
+    async def mount(self, mount_select: bool = True, initial: bool = True) -> None:
+        self.init()
         if self.unknown_tool:
             await self.process.mount(TextArea("unkdnown", True))
             self.process.children[0].styles.height = "auto"
             self.process.children[0].text = self.old_tool
             self.process.children[0].focus()
         else:
-            await self.process.mount(Markdown(f"### function: `{self.tool_name}`\n#### arguments:"))
-            for prop in self.properties.keys():
-                value = ""
-                if prop in self.arguments:
-                    value = self.arguments[prop]
-                await self.process.mount(Markdown(f"`{prop}`"))
-                if prop in self.required:
-                    await self.process.mount(TextArea(prop, True))
-                else:
-                    await self.process.mount(TextArea(prop))
-                self.process.children[-1].text = value
-                self.process.children[-1].styles.height = "auto"
-            self.process.children[2].focus()
+            if mount_select:
+                await self.process.mount(Label("\n[bold $accent]function:"))
+                await self.select_tool()
+            if initial:
+                return None
+            await self.process.mount(Label("\n[bold $accent-lighten-1]arguments:\n"))
+            async with self.process.batch():
+                for prop in self.properties.keys():
+                    value = ""
+                    await self.process.mount(Label(f"{prop}:"))
+                    if prop in self.arguments:
+                        value = self.arguments[prop]
+                    if prop in self.required:
+                        await self.process.mount(TextArea(prop, True))
+                    else:
+                        await self.process.mount(TextArea(prop))
+                    self.process.children[-1].text = value
+                    self.process.children[-1].styles.height = "auto"
+            self.process.children[4].focus()
 
     def save_unknown(self) -> bool:
         text = self.process.children[0].text
@@ -85,11 +103,14 @@ class TextAreaTool():
             if required and not text:
                 return False
             arguments[prop] = text
-        arguments = json.dumps(arguments)
+        save_arguments = {}
+        for prop in arguments:
+            if prop in self.properties.keys() and arguments[prop]:
+                save_arguments[prop] = arguments[prop]
+        self.tool["function"]["arguments"] = json.dumps(save_arguments)
         index = self.chat_view.messages.index(self.message.message)
         self.chat.undo.append_undo("change", self.message.message, index)
-        self.tool["function"]["arguments"] = arguments
-        self.message.message["tool_calls"][self.process.count]["function"]["arguments"] = arguments
+        self.message.message["tool_calls"][self.process.count] = self.tool
         return True
 
     async def save(self) -> None:
