90a1604b0441e0405e6805ad24cbc67d15682cb4
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-01 01:14:52 +0000

    chat: textual_message: add RemoveProcess

--- spit_app/chat/textual_message.py
+++ spit_app/chat/textual_message.py
@@ -10,3 +10,9 @@ class RemoveMessage(Message):
     def __init__(self, index: int) -> None:
         self.index = index
         super().__init__()
+
+class RemoveProcess(Message):
+    def __init__(self, scontent: str, index: int|None = None) -> None:
+        self.index = index
+        self.scontent = scontent
+        super().__init__()
