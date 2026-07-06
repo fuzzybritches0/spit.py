6a139069b458c7d4f35f8f41ad9da1c5e1910b88
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-06 12:08:24 +0000

    message: content: process: add action_copy()

--- spit_app/chat/message/content/process/actions.py
+++ spit_app/chat/message/content/process/actions.py
@@ -5,6 +5,7 @@ from .text_area_tool import TextAreaTool
 from spit_app.chat.textual_message import RemoveProcess
 
 bindings = [
+    ("c", "copy", "Copy"),
     ("e", "edit", "Edit"),
     ("x", "remove", "Rem."),
     ("ctrl+enter", "save", "Save"),
@@ -12,6 +13,16 @@ bindings = [
 ]
 
 class ActionsMixIn:
+    def action_copy(self) -> None:
+        if type(self.message.message[self.scontent]) is str:
+            self.app.copy_to_clipboard(self.message.message[self.scontent])
+        else:
+            _type = self.message.message[self.scontent][self.count]["type"]
+            if _type == "function":
+                self.app.copy_to_clipboard(json.dumps(self.message.message[self.scontent][self.count][_type]))
+            else:
+                self.app.copy_to_clipboard(self.message.message[self.scontent][self.count][_type])
+
     async def action_save(self) -> None:
         await self.edit.save()
 
@@ -45,12 +56,19 @@ class ActionsMixIn:
         await self.edit.mount(False, False)
 
     def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
-        if self.chat.is_working() or not self.chat_view.is_edit:
+        if self.chat.is_working() or self.message.is_removing:
             return False
+        if action == "copy":
+            if type(self.message.message[self.scontent]) is str and not self.message.message[self.scontent]:
+                return False
+            elif type(self.message.message[self.scontent]) is dict:
+                _type = self.message.message[self.scontent][self.count]["type"]
+                if not self.message.message[self.scontent][self.count][_type]:
+                    return False
         if action == "edit" or action == "remove":
-            if self.is_edit or self.message.is_removing:
+            if self.is_edit or not self.chat_view.is_edit:
                 return False
         if action == "save" or action == "cancel":
-            if not self.is_edit:
+            if not self.is_edit or not self.chat_view.is_edit:
                 return False
         return True
