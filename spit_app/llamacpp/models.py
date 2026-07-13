11dbc9fe3ff3225de64d68343c6267516199a038
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-13 09:15:40 +0000

    llamacpp: models: add Gemma-4 series models

---
+++ spit_app/llamacpp/models.py
@@ -0,0 +1,32 @@
+MODELS = [
+    {
+        "name": "Gemma-4-E2B-it",
+        "org": "google",
+        "model": "gemma-4-E2B-it-qat-q4_0-gguf",
+        "files": ["gemma-4-E2B_q4_0-it.gguf", "gemma-4-E2B-it-mmproj.gguf"]
+    },
+    {
+        "name": "Gemma-4-E4B-it",
+        "org": "google",
+        "model": "gemma-4-E4B-it-qat-q4_0-gguf",
+        "files": ["gemma-4-E4B_q4_0-it.gguf", "gemma-4-E4B-it-mmproj.gguf"]
+    },
+    {
+        "name": "Gemma-4-12B-it",
+        "org": "google",
+        "model": "gemma-4-12B-it-qat-q4_0-gguf",
+        "files": ["gemma-4-12b-it-qat-q4_0.gguf", "mmproj-gemma-4-12b-it-qat-q4_0.gguf"]
+    },
+    {
+        "name": "Gemma-4-26B-A4B-it",
+        "org": "google",
+        "model": "gemma-4-26B-A4B-it-qat-q4_0-gguf",
+        "files": ["gemma-4-26B_q4_0-it.gguf", "gemma-4-26B-it-mmproj.gguf"]
+    },
+    {
+        "name": "Gemma-4-31B-it",
+        "org": "google",
+        "model": "gemma-4-31B-it-qat-q4_0-gguf",
+        "files": ["gemma-4-31B_q4_0-it.gguf", "gemma-4-31B-it-mmproj.gguf"]
+    }
+]
