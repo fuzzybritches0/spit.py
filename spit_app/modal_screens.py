f99e767d95ebdb059bdb200bb4cef3d1fd8c8b43
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-17 20:33:51 +0000

    modal_screen: Remove updating progress_state{}
    We already do this in Download. This leads to wrong states when the user
    dismisses the ProgressBarScreen and brings it back to the foreground
    later.

--- spit_app/modal_screens.py
+++ spit_app/modal_screens.py
@@ -44,15 +44,12 @@ class ProgressBarScreen(ModalScreen):
 
     def update_text(self, text: str) -> None:
         self.query_one("#text").update(text)
-        self.download.progress_state["text"] = text
 
     def update_total(self, total: int) -> None:
         self.query_one("#progress-bar").update(total=total)
-        self.download.progress_state["total"] = total
 
     def update_progress(self, advance: int) -> None:
         self.query_one("#progress-bar").advance(advance)
-        self.download.progress_state["progress"] += advance
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.control.id == "cancel":
