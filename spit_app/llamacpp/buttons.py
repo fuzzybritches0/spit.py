9dc8cdeea46aa6ad081b1cd10e19a2901d50bfdd
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-17 20:48:48 +0000

    llamacpp: move code to llamacpp.buttons [2/2]

--- spit_app/llamacpp/buttons.py
+++ spit_app/llamacpp/buttons.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+import platform
 from textual.widgets import Select
+from spit_app.textual_message import DownloadFiles
 
 class ButtonsMixIn:
     async def button_apply_llamacpp_settings(self) -> None:
@@ -29,7 +31,15 @@ class ButtonsMixIn:
             self.app.exception = Exception("Invalid version provided!")
             await self.update_input_llamacpp_version()
             return None
-        self.update_llamacpp(version)
+        machine = platform.uname().machine
+        if machine == "x86_64":
+            machine = "x64"
+        elif machine == "aarch64":
+            machine = "amd64"
+        file = f"llama-b{version}-bin-ubuntu-vulkan-{machine}.tar.gz"
+        url = f"https://github.com/ggml-org/llama.cpp/releases/download/b{version}/{file}"
+        lst = [(url, self.path["llamacpp"] / file)]
+        self.app.post_message(DownloadFiles(self, lst, "update_llamacpp"))
 
     async def button_delete_llamacpp(self) -> None:
         selection = self.query_one("#delete_version").value
