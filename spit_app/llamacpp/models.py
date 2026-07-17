1331bbf08254492068644324eb6ab642a69d8495
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-17 20:49:31 +0000

    llamacpp: models: add ids

--- spit_app/llamacpp/models.py
+++ spit_app/llamacpp/models.py
@@ -1,30 +1,35 @@
 MODELS = [
     {
         "name": "Gemma-4-E2B-it",
+        "id": "oq4LVj9KYJmczNlVkOH0paBrfI21NjAj",
         "org": "google",
         "model": "gemma-4-E2B-it-qat-q4_0-gguf",
         "files": ["gemma-4-E2B_q4_0-it.gguf", "gemma-4-E2B-it-mmproj.gguf"]
     },
     {
         "name": "Gemma-4-E4B-it",
+        "id": "YkCx4J7XuBMW3T2gVTuYc2Amj9aon5H6",
         "org": "google",
         "model": "gemma-4-E4B-it-qat-q4_0-gguf",
         "files": ["gemma-4-E4B_q4_0-it.gguf", "gemma-4-E4B-it-mmproj.gguf"]
     },
     {
         "name": "Gemma-4-12B-it",
+        "id": "DGbza622FFHxzwY75n1DB1PW3hkPa7zN",
         "org": "google",
         "model": "gemma-4-12B-it-qat-q4_0-gguf",
         "files": ["gemma-4-12b-it-qat-q4_0.gguf", "mmproj-gemma-4-12b-it-qat-q4_0.gguf"]
     },
     {
         "name": "Gemma-4-26B-A4B-it",
+        "id": "J8S1Wpe3mCcB8DFeg1b7ZA3jCZTERrZS",
         "org": "google",
         "model": "gemma-4-26B-A4B-it-qat-q4_0-gguf",
         "files": ["gemma-4-26B_q4_0-it.gguf", "gemma-4-26B-it-mmproj.gguf"]
     },
     {
         "name": "Gemma-4-31B-it",
+        "id": "zslSpCORNddn8CPeBr2zYsRJ9RFZdzRz",
         "org": "google",
         "model": "gemma-4-31B-it-qat-q4_0-gguf",
         "files": ["gemma-4-31B_q4_0-it.gguf", "gemma-4-31B-it-mmproj.gguf"]
