5b110d364915531021db9bccc333a5adcd611d34
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-19 11:42:39 +0000

    downloads: refactor, fix, and extend
    1. Add PostDownloadMethod:
      We need to separate post work that is connected with updating elements
      on the screen and work that is connected with post processing the
      download. Introduce PostDownloadMethod class to collect all methods we
      need to run for post-download processing.
    2. Allow cancelling with the option of resuming.
    3. Use sender_id instead of storing a Container class:
      We want to be able to store the download state, so the user can resume
      the pending downloads after an app restart. Introduce
      callback_sender() to retrieve the sender if it is mounted.
    4. Introduce post_download_method() to handle post-download methods from
       PostDownloadMethod.
    5. Refactor failure, cancel, and stop processing:
      We need to be careful here. We need to make sure we update the screen
      consistently and not get confused with the DownloadProgressScreen and
      ErrorScreen. Only show the ErrorScreen when we have dismissed the
      DownloadProgressScreen.

--- spit_app/download.py
+++ spit_app/download.py
@@ -1,9 +1,11 @@
 import os
 import httpx
 import asyncio
+import inspect
 from pathlib import Path
 from spit_app.modal_screens import ProgressBarScreen
 from spit_app.textual_message import DownloadFailed, DownloadSuccess
+from .post_download_methods import PostDownloadMethods as pdm
 
 class Download:
     def __init__(self, app) -> None:
@@ -13,6 +15,8 @@ class Download:
         self.settings = app.settings
         self.working = False
         self.cancel = False
+        self.clear = False
+        self.exception = None
         self.pending = []
         self.progress_bar_screen = None
         self.progress_state_reset()
@@ -40,12 +44,12 @@ class Download:
         else:
             self.progress_state[key] = value
 
-    async def cancel_work(self) -> None:
+    async def cancel_work(self, clear: bool = False) -> None:
+        self.clear = clear
         if self.working:
             self.cancel = True
             while self.working:
                 await asyncio.sleep(.1)
-            self.pending = []
         await self.progress_dismiss()
         self.progress_state_reset()
 
@@ -58,19 +62,39 @@ class Download:
         self.progress_bar_screen = ProgressBarScreen(self)
         self.push_screen(self.progress_bar_screen)
 
-    def download(self, sender, lst: list, callback: str) -> None:
-        self.pending.append((sender, lst, callback))
+    def download(self, sender_id: str, name: str, lst: list, callback: str) -> None:
+        self.pending.append((sender_id, name, lst, callback))
         if not self.progress_is_active():
             self.show_progress()
         if not self.working:
             self.run_worker(self.work_download())
+
+    def callback_sender(self, sender_id: str, callback: str, lst: list, success: bool) -> None:
+        for cont in self.app.query_one("#main").children:
+            if cont.id == sender_id:
+                if success:
+                    cont.post_message(DownloadSuccess(callback))
+                else:
+                    cont.post_message(DownloadFailed(callback))
+                return None
+
+    async def post_download_method(self, callback: str, lst: list, success: bool) -> None:
+        add = "failed"
+        if success:
+            add = "success"
+        if hasattr(pdm, f"{callback}_{add}"):
+            method = getattr(pdm, f"{callback}_{add}")
+            if inspect.iscoroutinefunction(method):
+                await method(self.app, lst)
+            else:
+                method(self.app, lst)
 
     async def work_download(self) -> None:
         self.working = True
         while self.pending:
             self.progress_state_reset()
             success = True
-            sender, lst, callback = self.pending[0]
+            sender_id, name, lst, callback = self.pending[0]
             count = 1
             total = len(lst)
             for url, path in lst:
@@ -78,16 +102,38 @@ class Download:
                 if self.progress_is_active():
                     file = str(path).split("/")[-1]
                     self.progress_update("text", f"Downloading {count} of {total}:\n{file}")
-                if not await self.try_download(url, path):
-                    success = False
-                    break
+                try:
+                    if not await self.try_download(url, path):
+                        success = False
+                        break
+                except Exception as exception:
+                    if type(exception).__name__ in ("ReadTimeout", "ConnectTimeout"):
+                        self.exception = exception
+                        success = False
+                        break
+                    else:
+                        raise self.exception
                 count += 1
             if success:
-                sender.post_message(DownloadSuccess(lst, callback))
-            else:
-                sender.post_message(DownloadFailed(lst, callback))
-            if self.pending:
                 del self.pending[0]
+                await self.post_download_method(callback, lst, True)
+                self.app.action_notify(f"Download finished for {name}")
+                self.callback_sender(sender_id, callback, lst, True)
+            else:
+                await self.post_download_method(callback, lst, False)
+                self.app.action_notify(f"Download failed for {name}")
+                self.callback_sender(sender_id, callback, lst, False)
+                if self.cancel:
+                    self.cancel = False
+                    if self.clear:
+                        self.pending = []
+                        self.clear = False
+                    self.working = False
+                    await self.progress_dismiss()
+                    if self.exception:
+                        self.app.exception = self.exception
+                        self.exception = None
+                    return None
         self.working = False
         await self.progress_dismiss()
 
@@ -118,6 +164,8 @@ class Download:
                 if resp.status_code == 416:
                     return True
                 if not resp.status_code == 200 and not resp.status_code == 206:
+                    await self.progress_dismiss()
+                    self.exception = Exception(str(resp))
                     return False
                 length = 0
                 if "Content-Length" in resp.headers:
@@ -125,14 +173,13 @@ class Download:
                 self.progress_update("total", length+size)
                 if not self.check_size(path, length+size):
                     file = str(path).split("/")[-1]
-                    e = f"Size of {file} changed on server! Please delete model and re-download!"
-                    self.exception = Exception(e)
+                    e = f"Size of {file} changed on server! Please delete and re-download!"
+                    await self.progress_dismiss()
+                    self.app.exception = Exception(e)
                     return False
                 self.progress_update("progress", size)
                 async for binary in resp.aiter_bytes():
                     if self.cancel:
-                        self.cancel = False
-                        self.working = False
                         return False
                     self.progress_update("progress", len(binary))
                     with open(path, "ab") as f:
