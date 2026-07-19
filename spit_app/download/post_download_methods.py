512dcd81014029149f16ce269ad86bd885deb7bf
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-19 13:43:42 +0000

    downloads:post_download_methods:remove on tar fail

--- spit_app/download/post_download_methods.py
+++ spit_app/download/post_download_methods.py
@@ -1,4 +1,5 @@
 import tarfile
+import shutil
 import os
 
 class PostDownloadMethods:
@@ -9,6 +10,11 @@ class PostDownloadMethods:
             tar.extractall(path=app.path["llamacpp"], filter="data")
             tar.close()
         except Exception as exception:
+                app.del_downloads_size([path])
+                version = path.split("/")[-1]
+                version = version.split("-")[1]
+                if os.path.isdir(app.path["llamacpp"] / f"llama-{version"):
+                    shutil.rmtree(app.path["llamacpp"] / f"llama-{version")
             app.exception = exception
         finally:
             try:
