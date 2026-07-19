844c96ad30858525c3b71582bc47758278a6ffdd
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-19 12:18:12 +0000

    llamacpp:callbacks: reduce code to screen updating

--- spit_app/llamacpp/callbacks.py
+++ spit_app/llamacpp/callbacks.py
@@ -1,30 +1,11 @@
-import tarfile
-import os
-
 class CallbacksMixIn:
-    async def update_llamacpp_failed(self, lst: list) -> None:
-        failed_version = self.query_one("#llamacpp_version").value
+    async def update_llamacpp_success(self) -> None:
+        await self.query_one("#active_version").method_update_options()
+        await self.query_one("#delete_version").method_update_options()
+
+    async def update_llamacpp_failed(self) -> None:
         await self.update_input_llamacpp_version()
-        self.app.action_notify(f"Failed to download {failed_version}!")
 
-    def update_llamacpp_success(self, lst: list) -> None:
-        _, path = lst[0]
-        try:
-            tar = tarfile.open(path)
-            tar.extractall(path=self.path["llamacpp"], filter="data")
-            tar.close()
-            self.update_options()
-        except Exception as exception:
-            self.app.exception = exception
-        finally:
-            try:
-                os.remove(path)
-            except:
-                pass
-        self.app.action_notify(f"Download finished!")
-
-    def download_model_success(self, lst: list) -> None:
-        self.app.action_notify(f"Download finished!")
-
-    def download_model_failed(self, lst: list) -> None:
-        self.app.action_notify(f"Download failed!")
+    async def download_model_success(self) -> None:
+        await self.query_one("#active_model").method_update_options()
+        await self.query_one("#download_model").method_update_options()
