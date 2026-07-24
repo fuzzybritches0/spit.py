014be36d22f647115d598a68833112a68be92b3f
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-24 16:54:32 +0000

    llamacpp: server: add Server class

---
+++ spit_app/llamacpp/server.py
@@ -0,0 +1,105 @@
+import os
+from .helpers import HelpersMixIn
+from .llamacpp import MANAGE
+
+class Server(HelpersMixIn):
+    def __init__(self, app) -> None:
+        self.manage = MANAGE
+        self.settings = app.settings
+        self.path = app.path
+        self.exception = app.exception
+        self.api_key = app.get_rand_seq(32)
+        self.applog = app.applog
+        self.preset = ""
+        self.log = ""
+        self.server = None
+        self.endpoint = None
+
+    def is_running(self) -> bool:
+        if self.server:
+            return True
+        return False
+
+    def compose_endpoint_settings(self) -> None:
+        port = self.gets("server_port")
+        timeout = self.gets("timeout")
+        cache_prompt = self.gets("cache_prompt")
+        self.endpoint = {
+            "endpoint_url": {"value": f"http://127.0.0.1:{port}/v1"},
+            "key": {"value": self.api_key},
+            "timeout": {"value": timeout},
+            "reasoning_key": {"value": "reasoning_content"},
+            "cache_pompt": {"value": cache_prompt}
+        }
+
+    def compose_server_arguments(self) -> list:
+        arguments = ["--models-preset", str(self.path["models"] / "models.ini"), "--no-ui",
+            "--no-ui-mcp-proxy", "--host", "127.0.0.1", "--port", str(self.gets("server_port")),
+            "--api-key", self.api_key]
+        devices = ""
+        if self.gets("vulkan_devices"):
+            for device in self.gets("vulkan_devices"):
+                devices += f"{device},"
+        if devices:
+            devices = devices[0:-1]
+            arguments += ["--device", devices]
+        else:
+            arguments += ["--device", "none"]
+        return arguments
+
+    def conc(self, line: str, clear: bool = False) -> None:
+        if clear:
+            self.preset = ""
+        self.preset += f"{line}\n"
+
+    def compose_preset(self) -> None:
+        self.conc("version = 1", True)
+        for model_id in self.gets("active_models"):
+            model_config = self.get_model(model_id)
+            model_setting = self.gets("server_settings", model_id)
+            model = None
+            draft = None
+            mmproj = None
+            for file in model_config["files"]:
+                if "draft" in file and not draft:
+                    draft = file
+                elif "mmproj" in file and not mmproj:
+                    mmproj = file
+                elif not model:
+                    model = file
+            if not model:
+                continue
+            self.conc(f"[{model_config['name']}]")
+            if model:
+                model_file = str(self.path["models"] / model_id / model)
+                self.conc(f"model = {model_file}")
+            if draft:
+                draft_file = str(self.path["models"] / model_id / draft)
+                self.conc(f"model-draft = {draft_file}")
+            if mmproj:
+                mmproj_file = str(self.path["models"] / model_id / mmproj)
+                self.conc(f"mmproj = {mmproj_file}")
+
+    def write_preset(self) -> None:
+        self.compose_preset()
+        with open(self.path["models"] / "models.ini", "w") as file:
+            file.write(self.preset)
+
+    async def start_server(self) -> None:
+        if not self.gets("active_version") or not self.gets("active_models"):
+            return None
+        llamacpp = self.path["llamacpp"] / ("llama-" + self.gets("active_version")) / "llama-server"
+        if not os.path.isfile(llamacpp):
+            self.puts("active_version", None)
+            self.settings.save()
+            return None
+        self.compose_endpoint_settings()
+        self.write_preset()
+        cmd = [str(llamacpp)]
+        cmd += self.compose_server_arguments()
+        async for line in self.run(cmd, "server"):
+            self.log += line
+
+    def stop_server(self) -> None:
+        self.stop(self.server)
+        self.server = None
