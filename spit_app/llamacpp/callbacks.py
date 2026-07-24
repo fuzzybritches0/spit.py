9ec52ba2db54ee2978b5cc63a659920a0de34a60
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-24 17:00:45 +0000

    llamacpp: put back active_version after refresh
    When we update the active_version list, put back the value that was set
    prior to the update if it is still in the list.

--- spit_app/llamacpp/callbacks.py
+++ spit_app/llamacpp/callbacks.py
@@ -2,6 +2,8 @@ class CallbacksMixIn:
     async def update_llamacpp_success(self) -> None:
         await self.query_one("#active_version").method_update_options()
         await self.query_one("#delete_version").method_update_options()
+        if self.gets("active_version") in self.get_versions_list():
+            self.query_one("#active_version").value = self.gets("active_version")
 
     async def update_llamacpp_failed(self) -> None:
         await self.update_input_llamacpp_version()
