a3574178c210d57430d4acc9041c79525a9fda9f
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-24 16:55:17 +0000

    app: stop server on exit

--- spit_app/actions.py
+++ spit_app/actions.py
@@ -48,6 +48,7 @@ class ActionsMixIn:
             return None
         self.confirm_exit = True
         if await self.push_screen_wait(ConfirmScreen()):
+            self.server.stop_server()
             self.settings.save()
             await self.download.cancel_work()
             for cont in self.query_one("#main").children:
