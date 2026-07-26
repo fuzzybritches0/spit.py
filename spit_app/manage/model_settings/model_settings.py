54a0580d8704457d9d837cc03d7c93c15eda1032
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-26 15:58:35 +0000

    manage: model_settings: introduce action_reset
    When the user edits a preset model setting, give them the option to
    reset their changes. Also, bring back the two lines - cleaned-up - we
    forgot to include in the last commit.

--- spit_app/manage/model_settings/model_settings.py
+++ spit_app/manage/model_settings/model_settings.py
@@ -1,9 +1,11 @@
+from copy import deepcopy
 from spit_app.manage.endpoint.common import Common, bindings, buttons
 from spit_app.manage.endpoint.actions import ActionsMixIn
 from spit_app.manage.endpoint.handlers import HandlersMixIn
 from spit_app.manage.endpoint.screens import ScreensMixIn
 from spit_app.manage.endpoint.validation import ValidationMixIn
 from spit_app.manage.manage import Manage
+from spit_app.llamacpp.models import MODELS_SETTINGS
 
 class ModelSettings(Common, ActionsMixIn, HandlersMixIn, ScreensMixIn, ValidationMixIn, Manage):
     NEW = {
@@ -19,3 +21,27 @@ class ModelSettings(Common, ActionsMixIn, HandlersMixIn, ScreensMixIn, Validatio
 
     def __init__(self) -> None:
         super().__init__("model-settings")
+        self.managed = self.settings.models
+        self.save_managed = self.settings.save_models
+
+    async def action_reset(self) -> None:
+        del self.settings.models[self.uuid]
+        self.settings.save_models()
+        self.settings.load_models()
+        self.managed = self.settings.models
+        self.manage = deepcopy(self.managed[self.uuid])
+        await self.remove_children()
+        await self.edit_manage_screen()
+
+    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool|None:
+        if self.children and self.children[0].id == "option-list":
+            return False
+        if action == "reset":
+            if not self.uuid in MODELS_SETTINGS:
+                return False
+        if action == "delete":
+            if self.uuid in MODELS_SETTINGS:
+                return False
+        if not action == "cancel" and not action == "save" and self.new_manage:
+            return False
+        return True
