6b7e825feecc398a24f6115b2e170573b91b60fc
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-26 16:02:16 +0000

    settings: extend saving and loading model settings
    We include the preset model settings so the user can choose from them.
    When we load the users model settings, check if they already have a
    model setting with the same name, and if so, omit adding the preset to
    the users model settings. Only store the differences of the user changes
    and the preset model setting. This way, we can update the presets
    values, should we need to in the future.

--- spit_app/settings.py
+++ spit_app/settings.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 from platformdirs import user_config_dir, user_data_dir
 from pathlib import Path
+from copy import deepcopy
+from .llamacpp.models import MODELS_SETTINGS
 
 class Settings:
     def __init__(self, app) -> None:
@@ -47,7 +49,39 @@ class Settings:
         self.app.write_json("endpointsf", self.endpoints)
 
     def save_models(self) -> None:
-        self.app.write_json("modelsf", self.models)
+        models = deepcopy(self.models)
+        deletes = []
+        for model_id in models.keys():
+            if model_id in MODELS_SETTINGS:
+                model = deepcopy(models[model_id])
+                for setting in models[model_id].keys():
+                    if models[model_id][setting]["value"] == MODELS_SETTINGS[model_id][setting]["value"]:
+                        del model[setting]
+                if not model:
+                    deletes += [model_id]
+                else:
+                    models[model_id] = model
+        if deletes:
+            for delete in deletes:
+                del models[delete]
+        self.app.write_json("modelsf", models)
+
+    def load_models(self) -> None:
+        if self.path["modelsf"].exists():
+            self.models = self.app.read_json("modelsf")
+        names = []
+        for model_id in self.models.keys():
+            if "name" in self.models[model_id]:
+                names += [self.models[model_id]["name"]["value"]]
+        for model_id in MODELS_SETTINGS.keys():
+            if not MODELS_SETTINGS[model_id]["name"]["value"] in names:
+                if not model_id in self.models:
+                    self.models[model_id] = deepcopy(MODELS_SETTINGS[model_id])
+                else:
+                    model = deepcopy(MODELS_SETTINGS[model_id])
+                    for setting in self.models[model_id].keys():
+                        model[setting] = self.models[model_id][setting]
+                    self.models[model_id] = deepcopy(model)
 
     def save_prompts(self) -> None:
         self.app.write_json("promptsf", self.prompts)
@@ -75,7 +109,6 @@ class Settings:
                 self.downloads = settings["downloads"]
         if self.path["endpointsf"].exists():
             self.endpoints = self.app.read_json("endpointsf")
-        if self.path["modelsf"].exists():
-            self.models = self.app.read_json("modelsf")
+        self.load_models()
         if self.path["promptsf"].exists():
             self.prompts = self.app.read_json("promptsf")
