703b1f8de4f921ad3562724f159f61e50a6dcdf2
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-07-30 14:57:31 +0000

    process: tool_call: fix de-escaping

--- spit_app/chat/message/content/process/tool_call.py
+++ spit_app/chat/message/content/process/tool_call.py
@@ -10,6 +10,10 @@ class ToolCall:
         self.key = False
         self.value = False
         self.json = []
+        self.unesc_pos = 0
+        self.unesc_skip = False
+        self.unesc_last_char = ""
+        self.unesc_tool_call = ""
 
     def tool_call_arguments(self) -> None:
         arguments = self.tool_call["arguments"]
@@ -57,11 +61,30 @@ class ToolCall:
             self.skip = False
         self.pos = len(arguments)
         self.formatted_tool_call += ret
-        return self.escaped()
+        return self.unescaped()
 
-    def escaped(self) -> str:
-        ret = self.formatted_tool_call.rstrip("\\")
-        ret = ret.replace(r'\"', '"')
-        ret = ret.replace("\\n", "\n")
-        ret = ret.replace("\\t", "\t")
-        return ret.replace("\\\\", "\\")
+    def unescaped(self) -> str:
+        newlines = self.formatted_tool_call.rstrip(r"\\")
+        newlines = newlines.replace(r"\n", "\n")
+        newlines = newlines.replace(r"\t", "\t")
+        newlines = newlines.replace(r"\\\\", "\\")
+        newlines = newlines.replace(r'\"', '"')
+        ret = ""
+        for pos in range(self.unesc_pos, len(newlines)):
+            char = newlines[pos:pos+1]
+            if self.unesc_last_char == "\\" and char == "\n":
+                self.unesc_skip = True
+                ret = ret[:-1] + r"\n"
+            if self.unesc_last_char == "\\" and char == "\t":
+                self.unesc_skip = True
+                ret = ret[:-1] + r"\t"
+            if self.unesc_last_char == "\\" and char == "\\":
+                self.unesc_skip = True
+                ret = ret[:-1] + r"\\"
+            if not self.unesc_skip:
+                ret += char
+            self.unesc_skip = False
+            self.unesc_last_char = char
+        self.unesc_tool_call += ret
+        self.unesc_pos = len(newlines)
+        return self.unesc_tool_call
