4b7b9e5191696b9e57f0f3bf02dd7f54fb0ec198
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-13 01:20:02 +0000

    manage: chat: focus() not load(), no messages yet

--- spit_app/manage/chat/actions.py
+++ spit_app/manage/chat/actions.py
@@ -21,7 +21,7 @@ class ActionsMixIn:
                 await self.app.maybe_reload("manage-chat")
             self.app.query_one("#side-panel").option_list()
             await self.app.query_one("#main").mount(Chat(f"chat-{self.uuid}"))
-            await self.app.query_one("#main").query_one(f"#chat-{self.uuid}").chat_view.load()
+            self.app.query_one("#main").query_one(f"#chat-{self.uuid}").focus()
         else:
             await super().after_action(action)
 
