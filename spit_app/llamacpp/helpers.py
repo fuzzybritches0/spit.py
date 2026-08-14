dc30e99e6ed42bf6b22334261516e71d7a546226
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-14 12:27:18 +0000

    llamacpp: refactor get_latest_llamacpp_version()

--- spit_app/llamacpp/helpers.py
+++ spit_app/llamacpp/helpers.py
@@ -207,26 +207,30 @@ class HelpersMixIn:
             await proc.wait()
             proc = None
 
+    def get_last_llamacpp_version(self) -> int:
+        if "latest" in self.settings.llamacpp:
+            return self.settings.llamacpp["latest"]
+        else:
+            return -1
+
     async def get_latest_llamacpp_version(self) -> int:
         if "latest" in self.settings.llamacpp and "latest_time" in self.settings.llamacpp:
             if time.time() < self.settings.llamacpp["latest_time"] + 3600:
                 return self.settings.llamacpp["latest"]
         url = "https://github.com/ggml-org/llama.cpp/releases/latest"
-        async with httpx.AsyncClient(timeout=15) as client:
-            async with client.stream("GET", url, follow_redirects=True) as resp:
-                if resp.status_code != 200:
-                    return -1
-                async for line in resp.aiter_lines():
-                    if "<title>" in line:
-                        line = line.strip()
-                        try:
+        try:
+            async with httpx.AsyncClient(timeout=15) as client:
+                async with client.stream("GET", url, follow_redirects=True) as resp:
+                    if resp.status_code != 200:
+                        return self.get_last_llamacpp_version()
+                    async for line in resp.aiter_lines():
+                        if "<title>" in line:
+                            line = line.strip()
                             version = int(line.split(" ")[1][1:])
-                        except:
-                            version = 0
-                        finally:
-                            if not version == 0:
-                                self.settings.llamacpp["latest"] = version
-                                self.settings.llamacpp["latest_time"] = time.time()
-                                self.settings.save()
+                            self.settings.llamacpp["latest"] = version
+                            self.settings.llamacpp["latest_time"] = time.time()
+                            self.settings.save()
                             return version
-        return -2
+        except:
+            return self.get_last_llamacpp_version()
+        return self.get_last_llamacpp_version()
