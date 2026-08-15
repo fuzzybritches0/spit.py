7d4d3fe96e29fb791c60d3b736e08d1a3e065d7f
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-15 13:15:15 +0000

    tools: terminal: refactor code
    1. Remove action, keys, enter, and literal properties.
    2. Use integer based indexes.
    3. Capture screen every time tool gets called.
    4. Return 'id' with every screen capture.
    5. Call run.term_input for every element in 'input' and let the method
       decide what to do with it.

--- spit_app/tools/terminal.py
+++ spit_app/tools/terminal.py
@@ -1,4 +1,5 @@
 import time
+import json
 from spit_app.tools.run.run import Run
 
 NAME = __file__.split("/")[-1][:-3]
@@ -11,50 +12,29 @@ DESC = {
         "parameters": {
             "type": "object",
             "properties": {
-                "action": {
-                    "type": "string",
-                    "description": "Available actions: new, send_keys, screen, kill."
-                },
                 "id": {
-                    "type": "string",
-                    "description": "The id of the terminal session."
+                    "type": "integer",
+                    "description": "The id of an existing terminal session."
                 },
-                "keys": {
-                    "type": "string",
-                    "description": "The keys to send to the terminal session."
-                },
-                "enter": {
-                    "type": "boolean",
-                    "description": "Also send 'Enter' key after 'keys'. Default: True"
-                },
-                "literal": {
-                    "type": "boolean",
-                    "description": "Send all 'keys' as literals. Default: True"
+                "input": {
+                    "type": "array",
+                    "description": "Send any string of characters or keys to a terminal session."
                 },
                 "delay": {
                     "type": "integer",
-                    "description": "Delay action screen by the given amount of seconds."
+                    "description": "Delay before capturing the terminal screen (seconds). Default: 1"
                 }
-            },
-            "required": ["action"]
+            }
         }
     }
 }
 
 SANDBOX = True
-PROMPT = """Use this function to control an interactive terminal session.
-
-Descriptions of actions:
-- new: Create a new terminal session. An 'id' will be returned for interacting with the session.
-- send_keys: Send 'keys' to the terminal session. Examples: "This is text", "echo "Hello World!", ...
-  If you want to send a key or key combinations like "C-c", "Up", "Left", ..., set parameter 'literal' to True.
-- screen: Return the current state of the terminal screen with the cursor displayed as '█'.
-- kill: Kill the terminal session with the 'id'.
-
-Parameter 'id' is mandatory for actions 'send_keys', 'screen', 'kill'.
-Parameter 'keys' is mandatory for action 'send_keys'.
-Parameters 'enter' and 'literal' are optional for action 'send_keys'.
-Parameter 'delay' is optional for action 'screen'.
+PROMPT = """
+The 'input' argument expects an array of strings of characters and/or keys that will be sent to the terminal session. The following keys are understood: Up, Down, Left, Right, Space, Tab, Delete, End, Enter, Escape, F1 ... F12, Home, Insert, PageDown, PageUp. To use key combinations, prefix them with 'C-' (Ctrl), 'S-' (Shift), 'M-' (Alt). Examples: ["echo \"Hello World!\"", "Enter"], ["vim test.txt", "Enter", "iHello test.txt file!", "Escape", ":wq", "Enter"]
+If you omit 'id', a new session will be created. You can not pick an 'id'. Every screen capture is prepended with the its 'id'.
+With at least the 'id' argument, a capture of the screen will be returned.
+Quit all applications and end the session with ["exit", "Enter"] if you no longer need it!
 """
 
 
@@ -64,29 +44,27 @@ SETTINGS = {
 
 def call(app, arguments: dict, chat_id) -> str:
     run = Run(app, chat_id, "", "", True, 0)
-    if not "action" in arguments or not arguments["action"]:
-        return "ERROR: No action!"
-    action = arguments["action"]
-    if action == "new":
-        return run.term_new()
     if not "id" in arguments or not arguments["id"]:
-        return f"ERROR: No id for action {arguments['action']}!"
-    idx = arguments["id"]
-    if action == "screen":
-        if "delay" in arguments and arguments["delay"]:
-            try:
-                delay = int(arguments["delay"])
-            except:
-                delay = 1
-            time.sleep(delay)
-        return run.term_screen(idx)
-    if action == "kill":
-        return run.term_kill(idx)
-    if action == "send_keys":
-        if not "keys" in arguments or not arguments["keys"]:
-            return "ERROR: argument keys missing!"
-        keys = arguments["keys"]
-        enter = arguments.get("enter", True)
-        literal = arguments.get("literal", True)
-        return run.term_send_keys(idx, keys, enter, literal)
-    return f"ERROR: Action {action} is not understood!"
+        idx = run.term_new()
+    else:
+        idx = arguments["id"]
+    panes = {}
+    if chat_id in app.tmux and "panes" in app.tmux[chat_id]:
+        panes = app.tmux[chat_id]["panes"]
+    if not type(idx) is int:
+        return f"ERROR: Session '{idx}' invalid!"
+    if not idx in panes or not panes[idx]:
+        return f"ERROR: Session '{idx}' does not exist!"
+    if "input" in arguments and arguments["input"]:
+        if not type(arguments["input"]) is list:
+            return "ERROR: expected array for argument 'input'!"
+        for inp in arguments["input"]:
+            ret = run.term_input(idx, inp)
+            if ret:
+                return ret
+    delay = 1
+    if "delay" in arguments and arguments["delay"]:
+        if type(arguments["delay"]) is int:
+                dealy = arguments["delay"]
+    time.sleep(delay)
+    return run.term_screen(idx)
