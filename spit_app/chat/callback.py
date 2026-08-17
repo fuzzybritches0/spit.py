97bc65f6e12dcdf266ab4669332f68bc73010dea
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-17 14:49:36 +0000

    chat: callback: make sure widgets are present
    Make sure widgets are present and use lock while working on them.

--- spit_app/chat/callback.py
+++ spit_app/chat/callback.py
@@ -5,15 +5,24 @@ class CallbackMixIn:
     def callback(self, message_index: int, signal: int) -> None:
         self.post_message(StreamCallback(message_index, signal))
 
+    def is_present(self, index: int) -> bool:
+        if index < len(self.children):
+            return True
+        return False
+
     async def message_finish(self, index: int) -> None:
         self.chat.write_chat_history()
         self.chat.undo.append_undo("insert", self.chat.messages[index], index)
-        await self.children[index].finish()
-        self.focus_message(index)
+        if self.is_present(index):
+            async with self.children[index].lock:
+                await self.children[index].finish()
 
     async def message_start(self, index: int) -> None:
         await self.mount(Message(self.chat, self.messages[index]))
-        self.focus_message(index)
+        if self.is_present(index):
+            async with self.children[index].lock:
+                await self.children[index].wait_for_refresh()
+                self.focus_message(index)
 
     def focus_message(self, index: int) -> None:
         if self.chat.display:
@@ -22,7 +31,10 @@ class CallbackMixIn:
             self.children[index].set_focused_message()
 
     async def message_process(self, index: int) -> None:
-        await self.children[index].process()
+        if self.is_present(index):
+            async with self.children[index].lock:
+                if self.display and (self.children[index].has_focus or self.children[index].has_focus_within):
+                    await self.children[index].process()
 
     async def on_stream_callback(self, message: StreamCallback) -> None:
         if message.signal == 0:
