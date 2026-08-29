95ba3d6077e30e6df1d4b18a894c84f846a0df40
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-29 21:25:59 +0000

    tools: file_info: use get_args()

--- spit_app/tools/file_info.py
+++ spit_app/tools/file_info.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 import json
-from spit_app.tools.run.run import Run, get_script
+from spit_app.tools.run.run import Run, get_script, get_args
 from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
@@ -42,10 +42,8 @@ EXEC = {
 
 async def call_async_generator(app, arguments: dict, chat_id):
     load_user_settings(app, NAME, SETTINGS)
-    script = f"""
-path = "{arguments['path']}"
-format = "{arguments.get('format', 'text')}"
-""" + EXEC["script"]
+    args = get_args(arguments, {"format": "text"})
+    script = args + EXEC["script"]
     run = Run(app, chat_id, EXEC["interpreter"], script,
               SETTINGS["sandbox"]["value"], 0)
     async for line in run.run():
