c7341f193cab63b89f19c2570cf87b48b596bf01
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-29 21:30:19 +0000

    tools: list_directories: use get_args()

--- spit_app/tools/list_directory.py
+++ spit_app/tools/list_directory.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-from spit_app.tools.run.run import Run, get_script
+from spit_app.tools.run.run import Run, get_script, get_args
 from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
@@ -49,12 +49,8 @@ EXEC = {
 
 async def call_async_generator(app, arguments: dict, chat_id):
     load_user_settings(app, NAME, SETTINGS)
-    script = f"""
-path = "{arguments['path']}"
-recursive = {arguments.get('recursive', False)}
-show_hidden = {arguments.get('show_hidden', False)}
-max_results = {arguments.get('max_results', 100)}
-""" + EXEC["script"]
+    args = get_args(arguments, {"recursive": False, "show_hidden": False, "max_results": 100})
+    script = args + EXEC["script"]
     run = Run(app, chat_id, EXEC["interpreter"], script,
               SETTINGS["sandbox"]["value"], 0)
     async for line in run.run():
