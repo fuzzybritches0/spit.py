c62482d3581c97131987368c84d826bf30d04cbf
Kurt Manucredo <fuzzybritches0@gmail.com>
2026-08-29 21:32:39 +0000

    tools: read_files: use get_args()

--- spit_app/tools/read_files.py
+++ spit_app/tools/read_files.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 import json
-from spit_app.tools.run.run import Run, get_script
+from spit_app.tools.run.run import Run, get_script, get_args
 from spit_app.tool_call import load_user_settings
 
 NAME = __file__.split("/")[-1][:-3]
@@ -48,15 +48,8 @@ EXEC = {
 
 async def call_async_generator(app, arguments: dict, chat_id):
     load_user_settings(app, NAME, SETTINGS)
-    encoding = arguments.get('encoding', 'utf-8')
-    path = arguments["path"]
-    if type(path) is str and not path.strip().startswith("["):
-        path = f'["{path}"]'
-    script = f"""
-path = {path}
-encoding = "{encoding}"
-show_line_numbers = {arguments.get('show_line_numbers', False)}
-""" + EXEC["script"]
+    args = get_args(arguments, {"encoding": "utf-8", "show_line_numbers": False})
+    script = args + EXEC["script"]
     run = Run(app, chat_id, EXEC["interpreter"], script,
               SETTINGS["sandbox"]["value"], 0)
     async for line in run.run():
